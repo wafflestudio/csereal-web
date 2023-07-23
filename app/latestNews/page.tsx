8e42af226ab0fef6875dbf3b4c88351e75883b63
Yeolyi <yeolyi1310@gmail.com>
2023-07-24 04:10:54 +0900

    ♻️ LocationLog 파일 제거

--- app/latestNews/page.tsx
+++ app/latestNews/page.tsx
@@ -1,14 +1,14 @@
+import { CurvedNode } from '@/components/Node';
 import PageTitle from '@/components/PageTitle';
 
 import { Location, PAGES } from '@/types/common';
 
-export default function NoticePage() {
+export default function LatestNewsPage() {
   const locationLog: Location[] = [PAGES.communication, PAGES.notice];
 
-  return (
-    <PageTitle
-      locationLog={locationLog}
-      title="푸른등대 2023gkrsuseh 2학기 학위논문제출기한 단순연장 학위논문제출기한"
-    />
-  );
+  return <PageTitle locationLog={locationLog}></PageTitle>;
+}
+
+function Title({ title }: { title: string }) {
+  return <h2>{title}</h2>;
 }
--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,21 +1,52 @@
+import Link from 'next/link';
+import { Fragment, ReactNode } from 'react';
+
 import { Location } from '@/types/common';
 
-import LocationLog from './LocationLog';
 import { CurvedNode } from './Node';
 
 interface PageTitleProps {
   locationLog: Location[];
-  title: string;
+  children: ReactNode;
 }
 
-export default function PageTitle({ locationLog, title }: PageTitleProps) {
+export default function PageTitle({ locationLog, children }: PageTitleProps) {
   return (
     <div className="w-fit min-w-[350px] max-w-[600px]">
       <div className="flex gap-2 mb-2">
         <LocationLog locations={locationLog} />
         <CurvedNode grow={true} />
       </div>
-      <h3 className="text-lg font-bold break-keep mr-[55px] font-yoon">{title}</h3>
+      {children}
     </div>
   );
+}
+
+function LocationLog({ locations }: { locations: Location[] }) {
+  return (
+    <div className="flex items-center gap-0.5">
+      {locations.map((loca, i) => {
+        return (
+          <Fragment key={loca.name}>
+            <LocationText path={loca.path} name={loca.name} />
+            {i !== locations.length - 1 && (
+              <span className="material-icons text-xs">arrow_forward_ios</span>
+            )}
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
+
+function LocationText({ path, name }: { path?: string; name: string }) {
+  if (path) {
+    return (
+      <Link href={path} className="text-xs font-yoon font-normal tracking-[.015em] hover:underline">
+        {name}
+      </Link>
+    );
+  } else {
+    return <span className="text-xs font-yoon font-normal tracking-[.015em]">{name}</span>;
+  }
 }
