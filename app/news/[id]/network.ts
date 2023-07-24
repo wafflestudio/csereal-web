37787ed8f36b071189c8fb7b4329519f5a0b65b9
이성열 <yeolyi1310@gmail.com>
2023-07-25 03:06:17 +0900

    ✨AdjPostNav 컴포넌트 추가
    * LocationLog에서 nav 태그를 사용하도록 변경
    
    * 피그마에 맞춰 색 추가 및 이름 변경
    
    * noto sans 500, 700 추가
    
    * material icons rounded import 추가
    
    * material symbols rounded를 가변 크기가 가능하도록 수정
    
    * AdjPostNav 컴포넌트 추가
    
    * Node에 변경된 색상 이름 적용
    
    * Material Icons를 Material Symbols Outlined로 변경
    
    * LocationLog nav에서 ol 태그로 변경 및 아이콘 교체

--- app/news/[id]/network.ts
+++ app/news/[id]/network.ts
@@ -1,4 +1,4 @@
-import { mock4 } from '@/components/common/HTMLViewer';
+import { mock1 } from '@/components/common/HTMLViewer';
 
 interface NoticePageProps {
   title: string;
@@ -12,7 +12,7 @@ export default function latestNewsNetwork(id: number): NoticePageProps {
   return {
     title: '김선 교수 연구진이 네트워크 사이언스와 머신러닝을 결합하여 약물에 적합한 질병을 예측',
     mainImageURL: '',
-    htmlContent: mock4,
+    htmlContent: mock1,
     postDate: new Date(),
   };
 }
--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,3 +1,4 @@
+import AdjPostNav from '@/components/common/AdjPostNav';
 import HTMLViewer from '@/components/common/HTMLViewer';
 import { StraightNode } from '@/components/common/Node';
 import PageTitle from '@/components/common/PageTitle';
@@ -43,19 +44,17 @@ function MainColumn({
       </PageTitle>
       <HTMLViewer htmlContent={htmlContent} />
       <StraightNode />
+      <AdjPostNav
+        nextPost={{
+          title:
+            '송현오 교수 연구진, 생성 모델의 이상 행동 탐지 기술 및 인공 신경망 깊이 압축 기술로 세계 선도',
+          href: './',
+        }}
+      />
     </div>
   );
 }
 
 function ShortcutColumn() {
   return <></>;
-}
-
-function Content({ content }: { content: string }) {
-  return (
-    <div
-      className="text-[.8125rem] font-yoon font-regular leading-8"
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
-  );
 }
