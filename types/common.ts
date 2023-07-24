006e454dc9b5849d40bf4933ed7b4806755dcc4b
Chansol Lim <lchans0319@gmail.com>
2023-07-24 18:35:58 +0900

    PageNames union type으로 변경

--- types/common.ts
+++ types/common.ts
@@ -3,20 +3,15 @@ export interface Location {
   path: string;
 }
 
-export enum PageNames {
-  main = 'main',
-  community = 'community',
-  notice = 'notice',
-  news = 'news',
-}
+export type PageNames = 'main' | 'community' | 'notice' | 'news';
+
+export type PagesUsingTag = 'notice' | 'news';
 
 // constants.ts를 따로 만들까 하다가 일단 여기에 넣어둡니다
 // 사실 '소식' 같은 건 페이지라기보단 탭인데 PAGES말고 TABS로 이름을 바꿔야 하나
 export const PAGES: { [key in PageNames]: Location } = {
-  [PageNames.main]: { name: '메인', path: '/' },
-  [PageNames.community]: { name: '소식', path: '' },
-  [PageNames.notice]: { name: '공지', path: '/notice' },
-  [PageNames.news]: { name: '새소식', path: '/news' },
+  main: { name: '메인', path: '/' },
+  community: { name: '소식', path: '' },
+  notice: { name: '공지', path: '/notice' },
+  news: { name: '새소식', path: '/news' },
 };
-
-export type PagesUsingTag = PageNames.notice | PageNames.news;
