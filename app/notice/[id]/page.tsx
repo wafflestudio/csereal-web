809047ae72a79619cdcf2d27d558e4ee02d525e3
Chansol Lim <lchans0319@gmail.com>
2023-07-25 14:27:27 +0900

    🩹 변경된 색깔 태그에 적용

--- app/notice/[id]/page.tsx
+++ app/notice/[id]/page.tsx
@@ -1,8 +1,28 @@
+import PageTitle from '@/components/common/PageTitle';
+import Sidebar from '@/components/common/Sidebar';
 import Tags from '@/components/common/Tags';
 
+import { Location, PAGES, TABS } from '@/types/common';
+
 export default function NoticePost() {
+  const locationLog: Location[] = [PAGES.community, PAGES.notice];
+  const subTabs: Location[] = TABS.community;
   const tags = ['연구', '장학'];
 
   const filterByTag = (tag: string) => {};
-  return <Tags tags={tags} page="notice" />;
+
+  return (
+    <>
+      <PageTitle locationLog={locationLog}>
+        <h3 className="text-lg font-bold break-keep font-yoon">
+          푸른등대 2023gkrsuseh 2학기 학위논문제출기한 단순연장 학위논문제출기한
+        </h3>
+      </PageTitle>
+      <div className="flex">
+        <div className="border w-[500px] h-[300px] mr-[30px]"></div>
+        <Sidebar mainTab={PAGES.community} currentTab={PAGES.notice} subTabs={subTabs} />
+      </div>
+      <Tags tags={tags} page="notice" />
+    </>
+  );
 }
--- components/common/Tags.tsx
+++ components/common/Tags.tsx
@@ -5,11 +5,12 @@ import { PagesAcceptingTagQuery } from '@/types/common';
 interface TagsProp {
   tags: string[];
   page: PagesAcceptingTagQuery;
+  margin?: string;
 }
 
-export default function Tags({ tags, page }: TagsProp) {
+export default function Tags({ tags, page, margin = '' }: TagsProp) {
   return (
-    <div className="flex items-center gap-2">
+    <div className={`flex items-center gap-2 ${margin}`}>
       {tags.map((tag) => (
         <Tag key={tag} tag={tag} page={page} />
       ))}
@@ -26,7 +27,7 @@ function Tag({ tag, page }: TagProp) {
   return (
     <Link
       href={{ pathname: `/${page}`, query: { tag: tag } }}
-      className="border rounded-[30px] border-orange text-orange px-2.5 py-0.5 h-[22px] text-xs"
+      className="border rounded-[30px] border-main-orange text-main-orange px-2.5 py-0.5 h-[22px] text-xs"
     >
       {tag}
     </Link>
