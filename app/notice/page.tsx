d0efe73fce47533995ad6e2f86d23e1b6ec562f1
Chansol Lim <lchans0319@gmail.com>
2023-07-25 15:07:09 +0900

    ✨ 검색바 구조 형성

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,4 +1,6 @@
+import Filter from '@/components/common/Filter';
 import PageTitle from '@/components/common/PageTitle';
+import SearchBar from '@/components/common/SearchBar';
 import Sidebar from '@/components/common/Sidebar';
 
 import { Location, PAGES, TABS } from '@/types/common';
@@ -10,12 +12,13 @@ export default function NoticePage() {
   return (
     <>
       <PageTitle locationLog={locationLog}>
-        <h3 className="text-lg font-bold break-keep font-yoon">
-          푸른등대 2023gkrsuseh 2학기 학위논문제출기한 단순연장 학위논문제출기한
-        </h3>
+        <h3 className="text-lg font-bold break-keep font-yoon">공지사항</h3>
       </PageTitle>
       <div className="flex">
-        <div className="border w-[500px] h-[300px] mr-[30px]"></div>
+        <div>
+          <Filter />
+          <SearchBar />
+        </div>
         <Sidebar mainTab={PAGES.community} currentTab={PAGES.notice} subTabs={subTabs} />
       </div>
     </>
---
+++ components/common/SearchBar.tsx
@@ -0,0 +1,17 @@
+interface SearchBarProps {
+  margin?: string;
+}
+
+export default function SearchBar({ margin = '' }: SearchBarProps) {
+  return (
+    <div className={`flex items-center gap-2.5 ${margin}`}>
+      <label htmlFor="search" className="font-yoon font-bold">
+        검색
+      </label>
+      <div className="flex items-center py-1 px-1.5 h-[30px] border rounded-[3px]">
+        <input type="text" id="search" className="outline-none" />
+        <span className="material-symbols-rounded text-xl mr-1.5">search</span>
+      </div>
+    </div>
+  );
+}
