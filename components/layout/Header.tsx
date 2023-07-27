8db06deb520359a7a643e63a3eeda9552a4dc035
Yeolyi <yeolyi1310@gmail.com>
2023-07-27 21:57:35 +0900

    Header 검색 로직 추가

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,37 +1,65 @@
 'use client';
 
-import { useState } from 'react';
+import { ChangeEvent, ChangeEventHandler, KeyboardEventHandler, useState } from 'react';
 
 export default function Header() {
-  const [text, setText] = useState('');
-
   return (
-    <header className="h-[8.75rem] mb-[20px] bg-neutral-200 sticky top-0 z-40 pt-12 px-[3.75rem] pb-[1.19rem] flex justify-between">
-      <div>
-        <a href="/">
-          <h2 className="font-yoon text-[1.375rem] font-bold tracking-[.03438rem] mb-[0.31rem] line-clamp-1">
-            서울대학교 컴퓨터공학부
-          </h2>
-          <h3 className="font-yoon text-[.875rem] font-bold tracking-[-0.04375rem] line-clamp-2">
-            Seoul National University
-            <br />
-            Dept. of Computer Science and Engineering
-          </h3>
-        </a>
-      </div>
-      <div className="flex flex-col justify-between items-end flex-grow">
-        <div>
-          <button className="font-yoon text-xs font-normal text-neutral-700">로그인</button>
-          <span className="font-yoon text-xs font-normal text-neutral-700 mx-[.62rem]">|</span>
-          <button className="font-yoon text-xs font-normal text-neutral-700">ENG</button>
-        </div>
-        <HeaderSearchBar text={text} setText={setText} />
-      </div>
+    <header className="h-[8.75rem] bg-white sticky top-0 z-40 pt-12 px-[3.75rem] pb-[1.19rem] flex justify-between">
+      <HeaderTitle />
+      <HeaderRight />
     </header>
   );
 }
 
-function HeaderSearchBar({ text, setText }: { text: string; setText: (text: string) => void }) {
+function HeaderTitle() {
+  return (
+    <div>
+      <a href="/">
+        <h2 className="font-yoon text-[1.375rem] font-bold tracking-[.03438rem] mb-[0.31rem] line-clamp-1">
+          서울대학교 컴퓨터공학부
+        </h2>
+        <h3 className="font-yoon text-[.875rem] font-bold tracking-[-0.04375rem] line-clamp-2">
+          Seoul National University
+          <br />
+          Dept. of Computer Science and Engineering
+        </h3>
+      </a>
+    </div>
+  );
+}
+
+function HeaderRight() {
+  //   const handleKeyDown = (event) => {
+
+  //   }
+
+  return (
+    <div className="flex flex-col justify-between items-end flex-grow">
+      <div>
+        <button className="font-yoon text-xs font-normal text-neutral-700">로그인</button>
+        <span className="font-yoon text-xs font-normal text-neutral-700 mx-[.62rem]">|</span>
+        <button className="font-yoon text-xs font-normal text-neutral-700">ENG</button>
+      </div>
+      <HeaderSearchBar />
+    </div>
+  );
+}
+
+function HeaderSearchBar() {
+  const [text, setText] = useState('');
+
+  const searchText = () => {
+    console.log(text + ' 검색');
+  };
+
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter') searchText();
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="flex items-center py-1 px-1.5 h-[30px] border rounded-[3px] max-w-[13.5rem] w-full">
       <input
@@ -39,9 +67,10 @@ function HeaderSearchBar({ text, setText }: { text: string; setText: (text: stri
         id="search"
         className="outline-none font-yoon text-xs w-full"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
-      <button>
+      <button onClick={searchText}>
         <span className="material-symbols-rounded text-xl mr-1.5">search</span>
       </button>
     </div>
