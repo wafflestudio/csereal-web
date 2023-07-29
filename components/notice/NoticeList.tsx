38af4f1d3ed7d259783cdb0c4ed4ebc6975a79f7
Chansol Lim <lchans0319@gmail.com>
2023-07-30 00:26:26 +0900

    💄 핀 아이콘 사용

--- components/notice/NoticeList.tsx
+++ components/notice/NoticeList.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+
+import pinIcon from '@/public/image/pin_icon.svg';
+
 interface NoticeListProps {}
 
 const NoticeMock = {
@@ -24,8 +28,8 @@ export default function NoticeList({}: NoticeListProps) {
   return (
     <div className="mt-3 mx-2.5 text-xs">
       <h5 className="h-[40px] pl-[50px] flex font-yoon border-b border-neutral-300 items-center">
-        <span className="w-[570px]">제목</span>
-        <span className="w-[200px]">날짜</span>
+        <span className="w-[570px] pl-3">제목</span>
+        <span className="w-[200px] pl-3">날짜</span>
       </h5>
       <ul className="">
         {noticeListMock.pin.map((notice, i) => (
@@ -56,9 +60,11 @@ function NoticeListRow({ notice, isPinned, idx }: NoticeListRowProps) {
 
   return (
     <li className={`flex items-center h-[40px] ${bgColor} ${fontWeight}`}>
-      <span className="w-[50px] text-center">{isPinned && 'pin'}</span>
-      <span className="w-[570px]">{notice.title}</span>
-      <span className="w-[200px]">{notice.date}</span>
+      <span className="w-[50px] px-[13px]">
+        {isPinned && <Image src={pinIcon} alt="고정글" width={24} />}
+      </span>
+      <span className="w-[570px] pl-3">{notice.title}</span>
+      <span className="w-[200px] pl-3">{notice.date}</span>
     </li>
   );
 }
