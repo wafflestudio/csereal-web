fb26dc6af20b988b29ff62c85beebe341d64f373
Yeolyi <yeolyi1310@gmail.com>
2023-08-03 04:12:43 +0900

    /news 레이아웃 정리

--- app/community/news/page.tsx
+++ app/community/news/page.tsx
@@ -1,51 +1,43 @@
-import AdjPostNav from '@/components/common/AdjPostNav';
-import HTMLViewer from '@/components/common/HTMLViewer';
+'use client';
+
+import { useState } from 'react';
+
 import { StraightNode } from '@/components/common/Nodes';
-import PageTitle from '@/components/common/PageTitle';
+import Pagination from '@/components/common/Pagination';
+import SearchForm from '@/components/common/search/SearchForm';
+import PageLayout from '@/components/layout/PageLayout';
 
 import { news } from '@/types/page';
+import { NewsTags } from '@/types/tag';
 
-import latestNewsNetwork from './[id]/network';
+import { useCustomSearchParams } from '@/utils/search';
+
+const POST_LIMIT = 20;
 
 export default function NewsPage({ params }: { params: { id: string } }) {
-  // 에러 처리 필요 id가 없으면?
-  const { title, mainImageURL, htmlContent, postDate } = latestNewsNetwork(+params.id);
+  const { page, keyword, tags, searchParams, setSearchParams } = useCustomSearchParams();
+  const [totalPostsCount, setTotalPostsCount] = useState<number>(0);
+  const pageTemp = page ? +page : 0;
+
+  const setCurrentPage = (pageNum: number) => {
+    setSearchParams({ purpose: 'navigation', page: pageNum });
+  };
 
   return (
-    <div className="flex flex-row pt-7 px-[3.75rem] pb-[5.37rem]">
-      <MainColumn title={title} htmlContent={htmlContent} postDate={postDate} />
-      <ShortcutColumn />
-    </div>
-  );
-}
-
-function MainColumn({
-  title,
-  htmlContent,
-  postDate,
-}: {
-  title: string;
-  htmlContent: string;
-  postDate: Date;
-}) {
-  return (
-    <div className="">
-      <PageTitle currentPage={news} margin="mb-[3.25rem]">
-        <h3 className="text-lg font-bold break-keep font-yoon">{title}</h3>
-      </PageTitle>
-      <HTMLViewer htmlContent={htmlContent} />
-      <StraightNode />
-      <AdjPostNav
-        nextPost={{
-          title:
-            '송현오 교수 연구진, 생성 모델의 이상 행동 탐지 기술 및 인공 신경망 깊이 압축 기술로 세계 선도',
-          href: './',
-        }}
+    <PageLayout currentPage={news} title="새 소식" titleSize="text-2xl">
+      <SearchForm
+        tags={NewsTags}
+        initTags={tags}
+        initKeyword={keyword ?? ''}
+        setSearchParams={setSearchParams}
       />
-    </div>
+      <StraightNode double={true} />
+      <Pagination
+        totalPostsCount={totalPostsCount}
+        postsCountPerPage={POST_LIMIT}
+        currentPage={pageTemp}
+        setCurrentPage={setCurrentPage}
+      />
+    </PageLayout>
   );
-}
-
-function ShortcutColumn() {
-  return <></>;
 }
