4687c5820927f192a7ca3a16dfcbb27fa38415fa
whwoohw <woojoo1114@naver.com>
2023-08-03 21:31:32 +0900

    ✨  학부 소개, 학회장 인사말 페이지 구현 (#12)
    * [feat] add basic directories
    
    * 에디터 컴포넌트 및 작성 페이지 구현
    
    * 뷰어 컴포넌트 및 페이지 구현
    
    * 학부 소개, 학부장 인사말 페이지  구현
    
    * 리뷰 반영
    
    * 뷰어 삭제
    
    * 뷰어 및 common.ts 삭제
    
    ---------
    
    Co-authored-by: Yeolyi <yeolyi1310@gmail.com>

---
+++ app/about/overview/edit/page.tsx
@@ -0,0 +1,55 @@
+'use client';
+import { Editor } from '@toast-ui/react-editor';
+import { useRef, useState } from 'react';
+
+import {
+  ContentEditor,
+  LanguageSelect,
+  ProfilePictureUpload,
+  TitleInput,
+} from '@/components/common/Editor/EditorOptions';
+import PageTitle from '@/components/common/PageTitle';
+import Sidebar from '@/components/common/Sidebar';
+
+import { overview } from '@/types/page';
+
+export default function EditorPage() {
+  const ref = useRef<Editor>(null);
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [language, setLanguage] = useState<'korean' | 'english'>('korean');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const markdownContent = ref.current?.getInstance().getMarkdown();
+    console.log(title);
+    console.log(markdownContent);
+  };
+
+  const handleImageSelect = (file: File | null) => {
+    console.log('Selected image:', file?.name);
+  };
+
+  return (
+    <div className="mx-10 mt-5 mb-40 h-screen">
+      <PageTitle title="학부 소개" currentPage={overview} textSize="text-lg" />
+      <div className="flex w-full h-full my-4">
+        <form onSubmit={handleSubmit} className="flex flex-col w-full mt-5 mr-10">
+          <LanguageSelect language={language} setLanguage={setLanguage} />
+          <ContentEditor content={content} editorRef={ref} />
+          <ProfilePictureUpload onImageSelect={handleImageSelect} />
+          <div className="flex flex-row-reverse">
+            <button
+              className="bg-neutral-600 text-white text-base rounded hover:bg-neutral-400 w-12 px-2 py-1 mt-5"
+              type="submit"
+            >
+              작성
+            </button>
+          </div>
+        </form>
+        <Sidebar currentTab={overview} />
+      </div>
+    </div>
+  );
+}
