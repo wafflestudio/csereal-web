8c9f7c0a406df06b6c388996f98e5fa79f0bf063
Yeolyi <yeolyi1310@gmail.com>
2023-08-03 15:46:13 +0900

    NewsRow에 Link 추가

--- components/news/NewsRow.tsx
+++ components/news/NewsRow.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { news } from '@/types/page';
 
@@ -22,16 +23,18 @@ export default function NewsRow({ title, description, tags, date, imageURL }: Ne
   return (
     <article className="text-neutral-700 font-noto flex pb-4 border-b-[1px] border-neutral-300 items-center">
       <div className="flex flex-col flex-1 mr-[3.75rem] break-keep">
-        <a href="">
-          <h3 className="text-md font-bold mb-[.69rem] hover:underline">{title}</h3>
-        </a>
-        <p className="text-xs font-normal mb-[.69rem] line-clamp-3">{description}</p>
+        <Link href="" className="hover:underline">
+          <h3 className="text-md font-bold mb-[.69rem]">{title}</h3>
+        </Link>
+        <Link href="" className="hover:cursor-pointer">
+          <p className="text-xs font-normal mb-[.69rem] line-clamp-3">{description}</p>
+        </Link>
         <Tags margin="mb-[.69rem]" tags={tags} page={news} />
         <time className="self-end text-xs font-normal">{dateStr}</time>
       </div>
-      <div className="h-[9.375rem] aspect-[4/3] relative">
+      <Link href="" className="h-[9.375rem] aspect-[4/3] relative">
         <Image alt="포스트 대표 이미지" src={imageURL} fill className="object-fill" />
-      </div>
+      </Link>
     </article>
   );
 }
