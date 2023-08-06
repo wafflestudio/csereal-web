24b4f24b43a2350c9c317289d23a13ce7fdf66f4
Yeolyi <yeolyi1310@gmail.com>
2023-08-07 00:59:57 +0900

    NewsRow 클릭하면 이동하는 기능 추가

--- components/news/NewsRow.tsx
+++ components/news/NewsRow.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link';
 
 import { news } from '@/types/page';
 
+import { getPath } from '@/utils/page';
+
 import Tags from '../common/Tags';
 
 export interface NewsRowProps {
+  href: string;
   title: string;
   description: string;
   tags: string[];
@@ -13,7 +16,7 @@ export interface NewsRowProps {
   imageURL: string;
 }
 
-export default function NewsRow({ title, description, tags, date, imageURL }: NewsRowProps) {
+export default function NewsRow({ href, title, description, tags, date, imageURL }: NewsRowProps) {
   description += '...'; // clip이 안될정도로 화면이 좌우로 긴 경우 대비
 
   const dateStr = date.toLocaleDateString('ko', {
@@ -26,16 +29,16 @@ export default function NewsRow({ title, description, tags, date, imageURL }: Ne
   return (
     <article className="text-neutral-700 font-noto flex pb-4 border-b-[1px] border-neutral-200 items-center">
       <div className="flex flex-col items-start flex-1 mr-[3.75rem] break-keep">
-        <Link href="" className="hover:underline">
+        <Link href={href} className="hover:underline">
           <h3 className="text-md font-bold mb-[.69rem]">{title}</h3>
         </Link>
-        <Link href="" className="hover:cursor-pointer">
+        <Link href={href} className="hover:cursor-pointer">
           <p className="text-xs font-normal mb-[.69rem] line-clamp-3">{description}</p>
         </Link>
         <Tags margin="mb-[.69rem]" tags={tags} page={news} />
         <time className="self-end text-xs font-normal">{dateStr}</time>
       </div>
-      <Link href="" className="h-[9.375rem] aspect-[4/3] relative">
+      <Link href={href} className="h-[9.375rem] aspect-[4/3] relative">
         <Image
           alt="포스트 대표 이미지"
           src={imageURL}
