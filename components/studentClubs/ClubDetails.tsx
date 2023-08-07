11b647ed564b7e2c45bf8d92b75609a59c552e49
Chansol Lim <lchans0319@gmail.com>
2023-08-07 14:41:25 +0900

    💄 노드 길이 조절

--- components/studentClubs/ClubDetails.tsx
+++ components/studentClubs/ClubDetails.tsx
@@ -1,3 +1,5 @@
+import { CSSProperties } from 'react';
+
 import HTMLViewer from '@/components/common/HTMLViewer';
 
 import { Club } from '@/types/club';
@@ -5,20 +7,23 @@ import { Club } from '@/types/club';
 import { StraightNode } from '../common/Nodes';
 
 export default function ClubDetails({ club }: { club: Club }) {
-  const nodeLength = `w-[840px-${club.image?.width ? club.image.width + 28 : 0}px)]`;
+  const len = `${club.image?.width ? club.image.width + 28 : 0}px`;
+  const nodeWidth: CSSProperties = {
+    width: `calc(100% - ${len})`,
+  };
 
   return (
     <div>
       <ClubTitle name={club.name} eng={club.eng} />
-      <StraightNode length={nodeLength} />
-      <HTMLViewer htmlContent={club.description} mainImage={club.image} margin="ml-2.5" />
+      <StraightNode style={nodeWidth} />
+      <HTMLViewer htmlContent={club.description} mainImage={club.image} margin="ml-2.5 mt-2" />
     </div>
   );
 }
 
 function ClubTitle({ name, eng }: { name: string; eng: string }) {
   return (
-    <h4 className="flex items-center gap-x-2.5 ml-2.5 h-10 font-noto text-neutral-700">
+    <h4 className="flex items-center gap-x-2.5 ml-2.5 mb-1 h-10 font-noto text-neutral-700">
       <span className="text-[1.25rem] font-black">{name}</span>
       <span className="text-md font-medium tracking-[0.02rem] pt-[0.1875rem]">{eng}</span>
     </h4>
