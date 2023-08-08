0712a6a525ea9a45ef48e0f8de08cc4f27d49d14
Chansol Lim <lchans0319@gmail.com>
2023-08-08 15:39:25 +0900

    ✨ 위치 주소 추가

--- app/about/directions/page.tsx
+++ app/about/directions/page.tsx
@@ -4,18 +4,16 @@ import { getDirections, getDirectionsMock } from '@/apis/directions';
 
 import HTMLViewer from '@/components/common/HTMLViewer';
 import DirectionsList from '@/components/directions/DirectionList';
+import LocationInformation from '@/components/directions/LocationInformation';
 import PageLayout from '@/components/layout/PageLayout';
 
-import { directions, staff } from '@/types/page';
-
-import { getPath } from '@/utils/page';
+import { directions } from '@/types/page';
 
 interface DirectionsPageProps {
   searchParams: { selected?: string };
 }
 
 const DEFAULT_DIRECITON = 'public-transit';
-const staffPath = getPath(staff);
 
 export default async function DirectionsPage({ searchParams }: DirectionsPageProps) {
   const selected = searchParams.selected ? decodeURI(searchParams.selected) : DEFAULT_DIRECITON;
@@ -23,16 +21,7 @@ export default async function DirectionsPage({ searchParams }: DirectionsPagePro
 
   return (
     <PageLayout currentPage={directions} title={directions.name} titleSize="text-2xl">
-      <p className="mb-[3.25rem] font-noto text-sm leading-6">
-        컴퓨터공학부는 서울대학교 관악 301동(신공학관1)에 있습니다.
-        <br />
-        주소: 08826 서울특별시 관악구 관악로 1 서울대학교 공과대학 컴퓨터공학부 행정실(301동 316호)
-        <br />
-        전화:{' '}
-        <Link href={staffPath} className="underline text-link">
-          학부 연락처
-        </Link>
-      </p>
+      <LocationInformation />
       <div>
         <DirectionsList directionsList={directionList} selectedDirections={selectedDirection} />
         {selectedDirection && <HTMLViewer htmlContent={selectedDirection.description} />}
---
+++ components/directions/LocationInformation.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+import { staff } from '@/types/page';
+
+import { getPath } from '@/utils/page';
+
+const staffPath = getPath(staff);
+
+export default function LocationInformation() {
+  return (
+    <div className="mb-[3.25rem]">
+      <LocationAddress />
+      <LocationMap />
+    </div>
+  );
+}
+
+function LocationAddress() {
+  return (
+    <p className="font-noto text-sm leading-6 mb-8">
+      컴퓨터공학부는 서울대학교 관악 301동(신공학관1)에 있습니다.
+      <br />
+      주소: 08826 서울특별시 관악구 관악로 1 서울대학교 공과대학 컴퓨터공학부 행정실(301동 316호)
+      <br />
+      전화:{' '}
+      <Link href={staffPath} className="underline text-link">
+        학부 연락처
+      </Link>
+    </p>
+  );
+}
+
+function LocationMap() {
+  return <div>map</div>;
+}
