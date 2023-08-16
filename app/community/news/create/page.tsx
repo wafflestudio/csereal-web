db4922af782f20b11e2369dd67e0f4e80b252737
Yeolyi <yeolyi1310@gmail.com>
2023-08-16 22:24:03 +0900

    SunEditor 설정

--- app/community/news/create/page.tsx
+++ app/community/news/create/page.tsx
@@ -1,16 +1,27 @@
-import { ReactNode } from 'react';
+'use client';
 
+import { ReactNode, useRef } from 'react';
+import SunEditorCore from 'suneditor/src/lib/core';
+
+import Editor from '@/components/editor/SunEditorWrapper';
 import PageLayout from '@/components/layout/PageLayout';
 
 export default function NewsCreate() {
+  const editorRef = useRef<SunEditorCore>();
+
   return (
     <PageLayout title="새소식 쓰기" titleType="small">
       <form className="flex flex-col">
         <Fieldset title="제목" mb="mb-6">
-          ㅁ
+          <input
+            type="text"
+            className={`mw-[40rem] rounded-sm border-[1px] border-neutral-700 h-[1.875rem] 
+            outline-none font-noto text-xs pl-2 font-normal `}
+            placeholder="제목을 입력하세요."
+          />
         </Fieldset>
         <Fieldset title="내용" mb="mb-6">
-          ㅁ
+          <Editor editorRef={editorRef} />
         </Fieldset>
         <Fieldset title="대표 이미지" mb="mb-6">
           ㅁ
@@ -18,12 +29,8 @@ export default function NewsCreate() {
         <Fieldset title="첨부파일" mb="mb-6">
           ㅁ
         </Fieldset>
-        <Fieldset title="태그" mb="mb-6">
-          ㅁ
-        </Fieldset>
-        <Fieldset title="게시 설정" mb="mb-6">
-          ㅁ
-        </Fieldset>
+        <Fieldset title="태그" mb="mb-6">ㅁ</Fieldset>
+        <Fieldset title="게시 설정" mb="mb-6">ㅁ</Fieldset>
       </form>
     </PageLayout>
   );
@@ -38,7 +45,7 @@ interface FieldsetProps {
 const Fieldset = ({ title, mb, children }: FieldsetProps) => {
   return (
     <fieldset className={`flex flex-col ${mb}`}>
-      <label className="font-yoon text-sm font-medium tracking-wide">{title}</label>
+      <label className="font-yoon text-sm font-medium tracking-wide mb-2">{title}</label>
       {children}
     </fieldset>
   );
---
+++ components/editor/SunEditorWrapper.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { MutableRefObject } from 'react';
+import { ko } from 'suneditor/src/lang/';
+import SunEditorCore from 'suneditor/src/lib/core';
+import plugins from 'suneditor/src/plugins';
+import SunEditor from 'suneditor-react';
+
+import 'suneditor/dist/css/suneditor.min.css';
+
+export default function Editor({
+  editorRef,
+}: {
+  editorRef: MutableRefObject<SunEditorCore | undefined>;
+}) {
+  return (
+    <SunEditor
+      getSunEditorInstance={(x) => (editorRef.current = x)}
+      height="400px"
+      lang={ko}
+      setOptions={{
+        plugins,
+        buttonList: [
+          ['undo', 'redo'],
+          ['font', 'fontSize', 'formatBlock'],
+          ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+          '/', // Line break
+          ['fontColor', 'hiliteColor'],
+          ['align', 'horizontalRule', 'list'],
+          ['table', 'link', 'image'],
+        ],
+      }}
+    />
+  );
+}
