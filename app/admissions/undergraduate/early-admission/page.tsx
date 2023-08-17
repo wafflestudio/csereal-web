1531129c59e81993dcf6ca0c03386b0aa7667f7d
Yeolyi <yeolyi1310@gmail.com>
2023-08-18 03:59:42 +0900

    getRootTab 수정

---
+++ app/admissions/undergraduate/early-admission/page.tsx
@@ -0,0 +1,5 @@
+import PageLayout from '@/components/layout/pageLayout/PageLayout';
+
+export default function EarlyAdmission() {
+  return <PageLayout titleType="small">asd</PageLayout>;
+}
---
+++ app/admissions/undergraduate/regular-admission/page.tsx
@@ -0,0 +1,5 @@
+import PageLayout from '@/components/layout/pageLayout/PageLayout';
+
+export default function RegularAdmission() {
+  return <PageLayout titleType="small">asd</PageLayout>;
+}
