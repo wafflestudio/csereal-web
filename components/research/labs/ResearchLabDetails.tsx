1742f811b8fecad5923a890643e31f6f4d369bee
Limchansol <lchans0319@gmail.com>
2023-08-19 23:07:43 +0900

    💄 연구그룹명 길이 조절

--- components/research/labs/ResearchLabDetails.tsx
+++ components/research/labs/ResearchLabDetails.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
-import Pentagon from '@/public/image/pentagon.svg';
+import PentagonLong from '@/public/image/pentagon_long.svg';
+import PentagonShort from '@/public/image/pentagon_short.svg';
 
 import HTMLViewer from '@/components/common/HTMLViewer';
 
@@ -25,19 +26,28 @@ export default function ResearchLabDetails({ lab }: { lab: ResearchLab }) {
 }
 
 const researchGroupsPath = getPath(researchGroups);
+const LENGTH_BOUNDARY = 10;
 
 function AffiliatedGroup({ groupName }: { groupName: string }) {
+  const width = groupName.length < LENGTH_BOUNDARY ? 'w-[10.875rem]' : 'w-[16.4375rem]';
+
   return (
     <div className="relative w-fit">
       <Link
         href={`${researchGroupsPath}?selected=${groupName}`}
-        className="absolute inline-block w-64 h-[42px] font-yoon text-sm text-center py-2.5 peer hover:text-white duration-300"
+        className={`absolute inline-block ${width} h-[42px] font-yoon text-sm text-center py-2.5 peer hover:text-white duration-300`}
       >
         <span className="font-yoon tracking-[-0.019em] text-center">{groupName} 연구그룹</span>
       </Link>
-      <div className="text-white peer-hover:text-main-orange">
-        <Pentagon className="duration-300" />
-      </div>
+      {groupName.length < LENGTH_BOUNDARY ? (
+        <div className="text-white peer-hover:text-main-orange">
+          <PentagonShort className="duration-300" />
+        </div>
+      ) : (
+        <div className="text-white peer-hover:text-main-orange">
+          <PentagonLong className="duration-300" />
+        </div>
+      )}
     </div>
   );
 }
