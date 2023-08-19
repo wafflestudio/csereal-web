e6c849ecfb7ca702691122711adf0670aac14b9b
Yeolyi <yeolyi1310@gmail.com>
2023-08-19 21:38:39 +0900

    Dropdown 리팩토링

--- components/common/Dropdown.tsx
+++ components/common/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from 'react';
+import { useReducer } from 'react';
 
 interface DropdownProps {
   contents: string[];
@@ -15,33 +15,68 @@ export default function Dropdown({ contents, selectedIndex, onClick }: DropdownP
 
   return (
     <div className="flex flex-col">
-      <button
-        className={`
-            flex items-center gap-4 py-[.3125rem] pr-[.3125rem] pl-[.625rem] border-neutral-300 border
-            ${expanded ? 'rounded-tl-sm rounded-tr-sm' : 'rounded-sm'}
-        `}
-        onClick={toggleExpanded}
-      >
-        <p className="text-sm font-normal">{contents[selectedIndex]}</p>
-        <span className="material-symbols-rounded text-base">
-          {expanded ? 'expand_less' : 'expand_more'}
-        </span>
-      </button>
       <div className="relative">
+        <DropdownButton
+          expanded={expanded}
+          toggleExpanded={toggleExpanded}
+          contents={contents}
+          selectedIndex={selectedIndex}
+        />
         {expanded && (
-          <div className="absolute top-0 left-0 right-0 border-l border-r border-b border-neutral-300 rounded-bl-sm rounded-br-sm bg-white">
-            {contents.map((content, index) => (
-              <button
-                key={index}
-                className="w-full text-left text-sm font-normal py-[.28rem] pl-[.62rem] hover:bg-neutral-100"
-                onClick={() => handleClick(index)}
-              >
-                {content}
-              </button>
-            ))}
+          <div className="relative">
+            <DropdownList contents={contents} handleClick={handleClick} />
           </div>
         )}
       </div>
+    </div>
+  );
+}
+
+function DropdownButton({
+  expanded,
+  toggleExpanded,
+  contents,
+  selectedIndex,
+}: {
+  expanded: boolean;
+  toggleExpanded: () => void;
+  contents: string[];
+  selectedIndex: number;
+}) {
+  return (
+    <button
+      className={`
+            flex items-center gap-4 py-[.3125rem] pr-[.3125rem] pl-[.625rem] border-neutral-300 border
+            ${expanded ? 'rounded-tl-sm rounded-tr-sm' : 'rounded-sm'}
+        `}
+      onClick={toggleExpanded}
+    >
+      <p className="text-sm font-normal">{contents[selectedIndex]}</p>
+      <span className="material-symbols-rounded text-base">
+        {expanded ? 'expand_less' : 'expand_more'}
+      </span>
+    </button>
+  );
+}
+
+function DropdownList({
+  contents,
+  handleClick,
+}: {
+  contents: string[];
+  handleClick: (index: number) => void;
+}) {
+  return (
+    <div className="absolute top-0 left-0 right-0 border-l border-r border-b border-neutral-300 rounded-bl-sm rounded-br-sm bg-white">
+      {contents.map((content, index) => (
+        <button
+          key={index}
+          className="w-full text-left text-sm font-normal py-[.28rem] pl-[.62rem] hover:bg-neutral-100"
+          onClick={() => handleClick(index)}
+        >
+          {content}
+        </button>
+      ))}
     </div>
   );
 }
