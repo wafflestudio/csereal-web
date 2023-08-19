5d636a2c564910e7c08a325f4cd8ed8f49d4c39c
Yeolyi <yeolyi1310@gmail.com>
2023-08-19 18:37:43 +0900

    졸업생 창업 기업 표 추가

--- app/about/future-careers/page.tsx
+++ app/about/future-careers/page.tsx
@@ -1,5 +1,7 @@
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
 
+const TABLE_COLUMN_SIZE = ['w-12', 'w-[12.5rem]', 'w-80', 'w-20'];
+
 export default async function GreetingsPage() {
   return (
     // TODO: 이전 PR 적용 필요 (marginBottom)
@@ -19,6 +21,64 @@ export default async function GreetingsPage() {
       </p>
       <h3 className="text-base font-bold leading-[1.625rem] mb-[0.8rem]">졸업생 진로 현황</h3>
       <h3 className="text-base font-bold leading-[1.625rem] mb-[0.8rem]">졸업생 창업 기업</h3>
+
+      <div className="text-xs font-normal border-y-[1px] border-neutral-200">
+        <CompanyTableHeader />
+        <ol>
+          {companies.map((company, index) => (
+            <CompanyTableRow key={index} index={index + 1} {...company} />
+          ))}
+        </ol>
+      </div>
     </PageLayout>
   );
 }
+
+function CompanyTableHeader() {
+  return (
+    <div className="flex pt-3 pb-[.81rem] border-b-[1px] border-neutral-200 [&_p]:pl-3 leading-3">
+      <p className={TABLE_COLUMN_SIZE[0]}>연번</p>
+      <p className={TABLE_COLUMN_SIZE[1]}>창업 기업명</p>
+      <p className={TABLE_COLUMN_SIZE[2]}>홈페이지</p>
+      <p className={TABLE_COLUMN_SIZE[3]}>창업연도</p>
+    </div>
+  );
+}
+
+interface CompanyTableRowProps {
+  index: number;
+  name: string;
+  url: string;
+  year: number;
+}
+
+function CompanyTableRow({ index, name, url, year }: CompanyTableRowProps) {
+  return (
+    <div className={`flex pt-[.63rem] pb-3 ${index % 2 && 'bg-neutral-50'}`}>
+      <p className={'pl-5 ' + TABLE_COLUMN_SIZE[0]}>{index}</p>
+      <p className={'pl-3 ' + TABLE_COLUMN_SIZE[1]}>{name}</p>
+      <a className={'text-link pl-3 ' + TABLE_COLUMN_SIZE[2]} href={url}>
+        {url}
+      </a>
+      <p className={'pl-5 ' + TABLE_COLUMN_SIZE[3]}>{year}</p>
+    </div>
+  );
+}
+
+const companies = [
+  {
+    name: '엔트로피패러독스 주식회사',
+    url: 'https://www.naver.com',
+    year: 2012,
+  },
+  {
+    name: '엔트로피패러독스 주식회사',
+    url: 'https://www.naver.com',
+    year: 2012,
+  },
+  {
+    name: '엔트로피패러독스 주식회사',
+    url: 'https://www.naver.com',
+    year: 2012,
+  },
+];
