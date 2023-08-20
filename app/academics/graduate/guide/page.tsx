72292564b2debe662fb144f74d126717f97a8566
Limchansol <lchans0319@gmail.com>
2023-08-21 05:17:22 +0900

    ✨ 대학원 안내 데이터 추가

--- app/academics/graduate/guide/page.tsx
+++ app/academics/graduate/guide/page.tsx
@@ -1,12 +1,18 @@
-import GuideContent from '@/public/image/undergraduate_guide_contents.svg';
+import GuideContent from '@/public/image/undergraduate_guide_contents.svg'; // 추후 수정
 
 import { getAcademicsGuide, getAcademicsGuideMock } from '@/apis/academics';
 
-import GuidePage from '@/components/academics/GuidePage';
+import HTMLViewer from '@/components/common/HTMLViewer';
+import PageLayout from '@/components/layout/pageLayout/PageLayout';
 
 export default async function GraduateGuidePage() {
   // const data = await getAcademicsGuide('graduate');
   const data = await getAcademicsGuideMock('graduate');
 
-  return <GuidePage description={data.description} Content={GuideContent} />;
+  return (
+    <PageLayout titleType="big" titleMargin="mb-9">
+      <GuideContent />
+      <HTMLViewer htmlContent={data.description} margin="mt-7" />
+    </PageLayout>
+  );
 }
--- app/academics/undergraduate/guide/page.tsx
+++ app/academics/undergraduate/guide/page.tsx
@@ -2,11 +2,17 @@ import GuideContent from '@/public/image/undergraduate_guide_contents.svg';
 
 import { getAcademicsGuide, getAcademicsGuideMock } from '@/apis/academics';
 
-import GuidePage from '@/components/academics/GuidePage';
+import HTMLViewer from '@/components/common/HTMLViewer';
+import PageLayout from '@/components/layout/pageLayout/PageLayout';
 
 export default async function UndergraduateGuidePage() {
-  //   const data = await getAcademicsGuide('undergraduate');
+  // const data = await getAcademicsGuide('undergraduate');
   const data = await getAcademicsGuideMock('undergraduate');
 
-  return <GuidePage description={data.description} Content={GuideContent} />;
+  return (
+    <PageLayout titleType="big" titleMargin="mb-9">
+      <GuideContent />
+      <HTMLViewer htmlContent={data.description} margin="mt-7" />
+    </PageLayout>
+  );
 }
