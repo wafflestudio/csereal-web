24d83f7a39d4a12d6f9a92d5bd05dbd878e8cef2
Limchansol <lchans0319@gmail.com>
2023-08-24 04:49:52 +0900

    💄 내용 맞춰서 width 늘리기

--- components/academics/CourseCard.tsx
+++ components/academics/CourseCard.tsx
@@ -1,8 +1,10 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 
 import { Tag } from '@/components/common/Tags';
 
 import { Course, SortOption } from '@/types/academics';
+
+import './CourseCard.css';
 
 interface CourseCardProps {
   course: Course;
@@ -19,30 +21,68 @@ const getSortedProperties = (course: Course, selectedOption: SortOption) => {
   }
 };
 
+const CARD_HEIGHT = 176;
+const LINE_LIMIT = 6;
+const TEXT_SIZE = 11;
+
 export default function CourseCard({ course, selectedOption }: CourseCardProps) {
   const sortedProperties = getSortedProperties(course, selectedOption);
   const [isFlipped, flipCard] = useReducer((x) => !x, false);
+  const frontRef = useRef<HTMLDivElement>(null);
+  const backRef = useRef<HTMLDivElement>(null);
 
-  return isFlipped ? (
-    <div
-      className="inline-block bg-neutral-100 h-44 px-[1.125rem] py-5 rounded shadow-[0_2px_4px_0_rgba(0,0,0,0.2)] cursor-pointer overflow-hidden "
-      onClick={flipCard}
-    >
-      <CardTitle name={course.name} code={course.code} />
-      <CardContent description={course.description} />
-    </div>
-  ) : (
-    <div
-      className="inline-block p-[1.125rem] h-44 rounded shadow-[0_2px_4px_0_rgba(0,0,0,0.2)] cursor-pointer"
-      onClick={flipCard}
-    >
-      <CardHeader sortedProperties={sortedProperties} />
-      <div className="inline-block">
+  if (backRef.current) {
+    const back = backRef.current;
+    if (back.scrollHeight > CARD_HEIGHT) {
+      const textCount = back.innerText.split('\n')[2].length;
+      const letterPerLine = textCount / LINE_LIMIT;
+      const expectedWidth = letterPerLine * TEXT_SIZE;
+      back.style.width = expectedWidth + 'px';
+    }
+  }
+
+  return (
+    <div className={`card ${isFlipped && 'flip'}`} onClick={flipCard}>
+      <div
+        className="front inline-block p-[1.125rem] h-44 rounded shadow-[0_2px_4px_0_rgba(0,0,0,0.2)] cursor-pointer"
+        ref={frontRef}
+      >
+        <CardHeader sortedProperties={sortedProperties} />
+        <div className="inline-block">
+          <CardTitle name={course.name} code={course.code} />
+          <CardContentPreview description={course.description} />
+        </div>
+      </div>
+      <div
+        className="back inline-block bg-neutral-100 h-44 px-[1.125rem] py-5 rounded shadow-[0_2px_4px_0_rgba(0,0,0,0.2)] cursor-pointer"
+        ref={backRef}
+      >
         <CardTitle name={course.name} code={course.code} />
-        <CardContentPreview description={course.description} />
+        <CardContent description={course.description} />
       </div>
     </div>
   );
+
+  //   return isFlipped ? (
+  //     <div
+  //       className="inline-block bg-neutral-100 h-44 px-[1.125rem] py-5 rounded shadow-[0_2px_4px_0_rgba(0,0,0,0.2)] cursor-pointer overflow-hidden "
+  //       onClick={flipCard}
+  //     >
+  //       <CardTitle name={course.name} code={course.code} />
+  //       <CardContent description={course.description} />
+  //     </div>
+  //   ) : (
+  //     <div
+  //       className="inline-block p-[1.125rem] h-44 rounded shadow-[0_2px_4px_0_rgba(0,0,0,0.2)] cursor-pointer"
+  //       onClick={flipCard}
+  //     >
+  //       <CardHeader sortedProperties={sortedProperties} />
+  //       <div className="inline-block">
+  //         <CardTitle name={course.name} code={course.code} />
+  //         <CardContentPreview description={course.description} />
+  //       </div>
+  //     </div>
+  //   );
 }
 
 function CardHeader({ sortedProperties }: { sortedProperties: string[] }) {
@@ -59,7 +99,7 @@ function CardHeader({ sortedProperties }: { sortedProperties: string[] }) {
 
 function CardTitle({ name, code }: { name: string; code: string }) {
   return (
-    <h2 className="mb-2">
+    <h2 className="mb-2 whitespace-nowrap">
       <span className="font-bold text-base mr-2">{name}</span>
       <span className="text-xs text-neutral-500">{code}</span>
     </h2>
@@ -68,18 +108,18 @@ function CardTitle({ name, code }: { name: string; code: string }) {
 
 function CardContentPreview({ description }: { description: string }) {
   return (
-    <p className="flex">
+    <div className="flex">
       <p className="grow w-0 line-clamp-2 text-[0.6875rem] text-neutral-400 leading-normal">
         {description}
       </p>
-    </p>
+    </div>
   );
 }
 
 function CardContent({ description }: { description: string }) {
   return (
-    <p className="flex">
+    <div className="flex">
       <p className="w-0 grow text-[0.6875rem] text-neutral-500">{description}</p>
-    </p>
+    </div>
   );
 }
