19903fbe8fb62cc0e70b806b5b7c72dd07f8d448
Limchansol <lchans0319@gmail.com>
2023-08-25 01:57:20 +0900

    ✨ 교과목 정보 페이지 추가 (#61)
    * ✨ 학사 및 교과 정렬 기준 추가
    
    * ✨ 교과목 정렬 함수 추가
    
    * 🚚  formatting함수들 각 컴포넌트로 이동
    
    * ✨ 교과목 카드 추가
    
    * 💄 내용 맞춰서 width 늘리기
    
    * 💄 flip 구현
    
    * 💄 스크롤 조정
    
    * 💄 flip 수정
    
    * 🐛 서브내비 이름 중복 버그 해결
    
    * 🐛 헤더 스크롤 수정
    
    * 🐛 스크롤시 전체 화면 움직이는 버그 해결
    
    * 🐛 flip 공간 남는 버그 수정
    
    * 💄 카드에 패딩 추가
    
    * 🔥 불필요한 파일 삭제
    
    * ✨ 대학원 교과목 정보 페이지 추가
    
    * ♻️ 리뷰 반영
    
    * ♻️ 리뷰 반영

--- components/common/Tags.tsx
+++ components/common/Tags.tsx
@@ -9,30 +9,52 @@ interface TagsProps {
 export default function Tags({ tags, margin = '', searchPath }: TagsProps) {
   return (
     <div className={`flex flex-wrap items-center gap-2.5 ${margin}`}>
-      {tags.map((tag) => (
-        <Tag key={tag} tag={tag} searchPath={searchPath} />
-      ))}
+      {searchPath
+        ? tags.map((tag) => (
+            <Link key={tag} href={`${searchPath}?tag=${tag}`} className="rounded-[1.875rem]">
+              <Tag tag={tag} hoverStyle="fill" />
+            </Link>
+          ))
+        : tags.map((tag) => <Tag key={tag} tag={tag} />)}
     </div>
   );
 }
 
+type HoverStyle = 'fill' | 'orange';
+type DefaultStyle = 'orange' | 'gray' | 'fill';
+
+const HOVER_STYLE: { [key in HoverStyle]: string } = {
+  fill: 'hover:bg-main-orange hover:border-main-orange hover:text-white',
+  orange: 'hover:bg-white hover:border-main-orange text-main-orange',
+};
+
+const DEFAULT_STYLE: { [key in DefaultStyle]: string } = {
+  orange: 'bg-white border-main-orange text-main-orange',
+  gray: 'bg-white border-neutral-400 text-neutral-400',
+  fill: 'bg-main-orange border-main-orange text-white',
+};
+
 interface TagProps {
   tag: string;
-  searchPath?: string;
+  hoverStyle?: HoverStyle;
+  defaultStyle?: DefaultStyle;
+  onClick?: (tag: string) => void;
 }
 
-function Tag({ tag, searchPath }: TagProps) {
-  const tagStyle =
-    'px-2.5 py-0.5 h-6 border rounded-[1.875rem] border-main-orange text-main-orange  text-xs whitespace-nowrap';
+export function Tag({ tag, hoverStyle, defaultStyle = 'orange', onClick }: TagProps) {
+  const tagClass =
+    'px-2.5 py-0.5 h-6 border rounded-[1.875rem] text-xs whitespace-nowrap cursor-default duration-200';
+  const defaultClass = DEFAULT_STYLE[defaultStyle];
+  const hoverClass = hoverStyle ? `${HOVER_STYLE[hoverStyle]} cursor-pointer` : '';
 
-  return searchPath ? (
-    <Link
-      className={`${tagStyle} hover:bg-main-orange hover:text-white duration-200`}
-      href={`${searchPath}?tag=${tag}`}
+  return (
+    <span
+      className={`${tagClass} ${defaultClass} ${hoverClass}`}
+      onClick={() => {
+        onClick && onClick(tag);
+      }}
     >
       {tag}
-    </Link>
-  ) : (
-    <span className={tagStyle}>{tag}</span>
+    </span>
   );
 }
