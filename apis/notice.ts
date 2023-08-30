45b98592f8d902faa0ba0dbc3230c215128744a2
Yeolyi <yeolyi1310@gmail.com>
2023-08-31 05:05:07 +0900

    GET post/[id] 적용

--- apis/notice.ts
+++ apis/notice.ts
@@ -1,26 +1,19 @@
-import { getMockNoticePostDetail, getMockNoticePosts } from '@/data/notice';
-
 import {
   NoticePostResponse,
   GETNoticePostsResponse,
   NoticePost,
-  SimpleNoticePost,
   PostSearchQueryParams,
 } from '@/types/post';
 
 import { deleteRequest, getRequest, patchRequest, postRequest } from '.';
-
-// export const getNoticePosts = getMockNoticePosts;
-
-export const getNoticePostDetail = getMockNoticePostDetail;
 
 const noticePath = '/notice';
 
 export const getNoticePosts = (params: PostSearchQueryParams) =>
   getRequest(noticePath, params) as Promise<GETNoticePostsResponse>;
 
-// export const getNoticePostDetail = (id: number, params: PostSearchQueryParams) =>
-//   getRequest(`${noticePath}/${id}`, params) as Promise<NoticePostResponse>;
+export const getNoticePostDetail = (id: number, params: PostSearchQueryParams) =>
+  getRequest(`${noticePath}/${id}`, params) as Promise<NoticePostResponse>;
 
 export const postNotice = (newPost: NoticePost) =>
   postRequest(noticePath, newPost) as Promise<NoticePostResponse>;
