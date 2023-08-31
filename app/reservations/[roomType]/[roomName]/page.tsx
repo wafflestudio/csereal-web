8fe72c3c46dc4bc580ae3d661cd57ed75b1959d2
Yeolyi <yeolyi1310@gmail.com>
2023-08-31 22:23:52 +0900

    시설 아이디와 시작 날짜 로직 구현

--- app/reservations/[roomType]/[roomName]/page.tsx
+++ app/reservations/[roomType]/[roomName]/page.tsx
@@ -1,21 +1,71 @@
+import { getWeeklyReservation } from '@/apis/reservation';
+
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
+import ReservationCalendar from '@/components/reservations/ReservationCalendar';
 
 interface RoomReservationProps {
   params: { roomType: string; roomName: string };
+  searchParams: { startDate?: string };
 }
 
-export default function RoomReservation({ params }: RoomReservationProps) {
-  const id = isValidRoomName(params.roomName) ? roomNameToId[params.roomName] : undefined;
+export default async function RoomReservation({ params, searchParams }: RoomReservationProps) {
+  const roomId = isValidRoomName(params.roomName) ? roomNameToId[params.roomName] : undefined;
+  const date = parseDate(searchParams.startDate || todayYMDStr());
+
+  if (roomId === undefined) {
+    return (
+      <PageLayout titleType="big" titleMargin="mb-[2.25rem]">
+        존재하지 않는 시설 아이디입니다.
+      </PageLayout>
+    );
+  }
+
+  if (date === undefined) {
+    return (
+      <PageLayout titleType="big" titleMargin="mb-[2.25rem]">
+        유효하지 않은 날짜입니다.
+      </PageLayout>
+    );
+  }
+
+  const reservations = await getWeeklyReservation(roomId, date.year, date.month, date.day);
 
   return (
     <PageLayout titleType="big" titleMargin="mb-[2.25rem]">
-      {id + ''}
+      <ReservationCalendar reservations={reservations} />
     </PageLayout>
   );
 }
 
 const isValidRoomName = (roomName: string): roomName is keyof typeof roomNameToId => {
   return Object.keys(roomNameToId).findIndex((x) => x === roomName) !== -1;
+};
+
+const todayYMDStr = () => {
+  const date = new Date();
+  return [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+    .map((x) => (x + '').padStart(2, '0'))
+    .join('-');
+};
+
+// yyyy-mm-dd 형식의 date의 유효성 검증
+const parseDate = (dateString: string) => {
+  // First check for the pattern
+  if (!/^\d{4}-\d{2}-\d{2}$/.test(dateString)) return undefined;
+
+  const [year, month, day] = dateString.split('-').map((x) => +x);
+
+  if (year < 1000 || year > 3000 || month == 0 || month > 12) return undefined;
+
+  const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  // 윤년
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) monthLength[1] = 29;
+
+  if (day > 0 && day <= monthLength[month - 1]) {
+    return { year, month, day };
+  } else {
+    return undefined;
+  }
 };
 
 const roomNameToId = {
---
+++ components/reservations/ReservationCalendar.tsx
@@ -0,0 +1,6 @@
+import { Reservation } from '@/types/reservation';
+
+export default function ReservationCalendar({ reservations }: { reservations: Reservation[] }) {
+  console.log(reservations);
+  return <p>ASD</p>;
+}
