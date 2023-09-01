c8a18f476fdf0d85b665dfc3c60faa974f46acc2
chansol <lchans0319@gmail.com>
2023-09-02 03:54:00 +0900

    ♻️ 타입추론 명확히 되도록 수정

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,7 @@ import SlideManagement from '@/components/admin/slide/SlideManagement';
 import SelectionList from '@/components/common/selection/SelectionList';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
 
-import { ADMIN_MENU, SimpleImportant, SimpleSlide } from '@/types/admin';
+import { ADMIN_MENU } from '@/types/admin';
 import { admin } from '@/types/page';
 
 import { getPath } from '@/utils/page';
@@ -20,10 +20,39 @@ const adminPath = getPath(admin);
 
 export default async function AdminPage({ searchParams: { selected, page } }: AdminPageProps) {
   const selectedMenu = selected ? replaceDashWithSpace(selected) : DEFAULT_MENU;
-  const { posts, total } =
-    selectedMenu === ADMIN_MENU.slide ? await getSlides() : await getImportants();
   const pageNum = (page && parseInt(page)) || 1;
 
+  if (selectedMenu === ADMIN_MENU.slide) {
+    const { posts, total } = await getSlides();
+    return (
+      <AdminPageLayout selectedMenu={selectedMenu}>
+        {posts && <SlideManagement posts={posts} total={total} page={pageNum} />}
+      </AdminPageLayout>
+    );
+  } else if (selectedMenu === ADMIN_MENU.important) {
+    const { posts, total } = await getImportants();
+    return (
+      <AdminPageLayout selectedMenu={selectedMenu}>
+        {posts && <ImportantManagement posts={posts} total={total} page={pageNum} />}
+      </AdminPageLayout>
+    );
+  } else {
+    return (
+      <AdminPageLayout selectedMenu={selectedMenu}>
+        <p>
+          <b>{`'${selectedMenu}'`}</b>은/는 존재하지 않는 메뉴입니다.
+        </p>
+      </AdminPageLayout>
+    );
+  }
+}
+
+interface AdminPageLayoutProps {
+  selectedMenu: string;
+  children: React.ReactNode;
+}
+
+function AdminPageLayout({ selectedMenu, children }: AdminPageLayoutProps) {
   return (
     <PageLayout title="관리자 메뉴" titleType="big" titleMargin="mb-9">
       <SelectionList
@@ -32,12 +61,7 @@ export default async function AdminPage({ searchParams: { selected, page } }: Ad
         path={adminPath}
         listGridColumnClass="grid-cols-[200px_220px]"
       />
-      {posts &&
-        (selectedMenu === ADMIN_MENU.slide ? (
-          <SlideManagement posts={posts as SimpleSlide[]} total={total} page={pageNum} />
-        ) : (
-          <ImportantManagement posts={posts as SimpleImportant[]} total={total} page={pageNum} />
-        ))}
+      {children}
     </PageLayout>
   );
 }
