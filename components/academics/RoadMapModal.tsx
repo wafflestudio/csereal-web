6fd58ad26da558079f7cfe33a577fd60c2da4d55
chansol <lchans0319@gmail.com>
2023-09-03 18:22:34 +0900

    💄 로드맵모달 크기 수정

--- components/academics/RoadMapModal.tsx
+++ components/academics/RoadMapModal.tsx
@@ -1,11 +1,11 @@
 import SignPostIcon from '@/public/image/signpost_icon.svg';
 
-import ModalFrame from '../modal/ModalFrame';
+import ModalFrame from '@/components/modal/ModalFrame';
 
 export default function RoadMapModal({ onClose }: { onClose: () => void }) {
   return (
     <ModalFrame onClose={onClose}>
-      <div className="relative flex flex-col gap-4 p-6 bg-neutral-100 border-t-2 border-b border-main-orange">
+      <div className="relative flex flex-col gap-4 p-6 w-fit bg-neutral-100 border-t-2 border-b rounded-t-sm border-main-orange overflow-auto styled-scrollbar overflow-x-hidden">
         <RoadMapTitle />
         <RoadMapDescription />
         <RoadMap />
@@ -29,17 +29,17 @@ function RoadMapTitle() {
 
 function RoadMapDescription() {
   return (
-    <p className="w-[45rem] text-sm text-neutral-700 leading-loose">
+    <p className="w-[70vw] max-w-[45rem] text-sm text-neutral-700 leading-loose">
       선수 교과목 그래프는 컴퓨터공학부 학부과정에서 어떤 전공과목을 수강하기에 앞서 자신이 과연
       수강하려 하는 전공과목에 대해 얼마나 많은 배경지식을 가지고 있는지를 확인해 볼 수 있게 해 주는
-      그래프 입니다. 어떤 과목을 수강하기 위해서는 그 과목으로 들어오는 화살표가 있는 과목들을 모두
+      그래프입니다. 어떤 과목을 수강하기 위해서는 그 과목으로 들어오는 화살표가 있는 과목들을 모두
       들어두는 것이 유리합니다.
     </p>
   );
 }
 
 function RoadMap() {
-  return <div className="h-[430px] bg-neutral-300"></div>;
+  return <div className="h-[430px] min-h-[15rem] bg-neutral-300"></div>;
 }
 
 function CloseButton({ onClick }: { onClick: () => void }) {
