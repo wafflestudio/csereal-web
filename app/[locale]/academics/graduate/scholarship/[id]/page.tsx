6ed51e691cfcc7198ad1026ae2cdf64dd75e417a
Yeolyi <yeolyi1310@gmail.com>
2023-09-10 17:35:00 +0900

    대학원 장학금 페이지 추가

---
+++ app/[locale]/academics/graduate/scholarship/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { getScholarship } from '@/apis/academicsServer';
+
+import HTMLViewer from '@/components/editor/HTMLViewer';
+import PageLayout from '@/components/layout/pageLayout/PageLayout';
+
+export default async function UndergraduateScholarshipPage({ params }: { params: { id: string } }) {
+  const { name, description } = await getScholarship(parseInt(params.id));
+
+  return (
+    // 타이틀이 긴 경우 정규표현식으로 괄호 내부 내용을 제거
+    // ex) 교외장학금 (현송문화재단, 유한재단, ...) -> 교외장학금
+    <PageLayout
+      title={name.length > 20 ? name.replace(/\([^)]*\)/g, '') : name}
+      titleType="big"
+      titleMargin="mb-8"
+    >
+      <HTMLViewer htmlContent={description} />
+    </PageLayout>
+  );
+}
