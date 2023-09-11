f98d5ed83e23288928d47d46f07aa28e5e144f55
chansol <lchans0319@gmail.com>
2023-09-12 00:39:27 +0900

    메인 중요 안내 추가

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,7 @@
+import ImportantPosts from '@/components/main/ImportantPosts';
 import ShortCutBox from '@/components/main/ShortCutBox';
 
+import { ImportantCategory } from '@/types/admin';
 import { facultyRecruitment, notice, topConferenceList } from '@/types/page';
 
 import { getPath } from '@/utils/page';
@@ -8,12 +10,25 @@ const tclPath = getPath(topConferenceList);
 const facultyRecruitmentPath = getPath(facultyRecruitment);
 const undergraduateNoticePath = getPath(notice) + '?tag=학부';
 
+const posts: {
+  category: ImportantCategory;
+  id: number;
+  title: string;
+  summary: string;
+}[] = [
+  { category: 'news', title: '뉴스 타이틀', summary: '어쩌고 저쩌고', id: 1 },
+  { category: 'notice', title: '공지스 타이틀', summary: '어쩌고 저쩌고', id: 4 },
+];
+
 export default function MainPage() {
   return (
     <div>
-      <ShortCutBox title={topConferenceList.name} href={tclPath} />
-      <ShortCutBox title={facultyRecruitment.name} href={facultyRecruitmentPath} />
-      <ShortCutBox title={'학부 ' + notice.name} href={undergraduateNoticePath} />
+      <div>
+        <ShortCutBox title={topConferenceList.name} href={tclPath} />
+        <ShortCutBox title={facultyRecruitment.name} href={facultyRecruitmentPath} />
+        <ShortCutBox title={'학부 ' + notice.name} href={undergraduateNoticePath} />
+      </div>
+      <ImportantPosts posts={posts} />
     </div>
   );
 }
---
+++ components/main/ImportantPosts.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+import { ImportantCategory } from '@/types/admin';
+
+interface ImportantPostsProps {
+  posts: ImportantPostMain[];
+}
+
+export default function ImportantPosts({ posts }: ImportantPostsProps) {
+  return (
+    <ul className="flex flex-col gap-6">
+      {posts.map((post) => (
+        <ImportantPostPreviewMain post={post} key={post.title} />
+      ))}
+    </ul>
+  );
+}
+
+interface ImportantPostMain {
+  category: ImportantCategory;
+  id: number;
+  title: string;
+  summary: string;
+}
+
+interface ImportantPostPreviewMainProps {
+  post: ImportantPostMain;
+}
+
+function ImportantPostPreviewMain({ post }: ImportantPostPreviewMainProps) {
+  return (
+    <li>
+      <Link href={`/community/${post.category}/${post.id}`}>
+        <h4 className="font-medium text-base tracking-wide">{post.title}</h4>
+        <p className="text-xs tracking-wide">{post.summary}</p>
+      </Link>
+    </li>
+  );
+}
