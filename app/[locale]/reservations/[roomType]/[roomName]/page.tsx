8f8b84193cdeddb27150a995cc45f02226d3d9fb
Yeolyi <yeolyi1310@gmail.com>
2023-09-15 20:55:05 +0900

    LoginStaffVisible, LoginUserVisible 추가

--- app/[locale]/reservations/[roomType]/[roomName]/page.tsx
+++ app/[locale]/reservations/[roomType]/[roomName]/page.tsx
@@ -1,5 +1,6 @@
 import { getWeeklyReservation, roomNameToId } from '@/apis/reservation';
 
+import LoginUserVisible from '@/components/common/LoginUserVisible';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
 import ReservationCalendar from '@/components/reservations/ReservationCalendar';
 
@@ -8,7 +9,15 @@ interface RoomReservationProps {
   searchParams: { selectedDate?: string };
 }
 
-export default async function RoomReservationPage({ params, searchParams }: RoomReservationProps) {
+export default function RoomReservationPage({ params, searchParams }: RoomReservationProps) {
+  return (
+    <LoginUserVisible fallback={<p>로그인 후 사용할 수 있는 페이지입니다.</p>}>
+      <LoginedRoomReservationPage params={params} searchParams={searchParams} />
+    </LoginUserVisible>
+  );
+}
+
+export async function LoginedRoomReservationPage({ params, searchParams }: RoomReservationProps) {
   const roomId = isValidRoomName(params.roomName) ? roomNameToId[params.roomName] : undefined;
   const date = parseDate(searchParams.selectedDate || todayYMDStr());
 
