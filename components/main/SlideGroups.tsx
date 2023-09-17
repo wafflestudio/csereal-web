1f6ae2a9d5b68b433d96bd99559055d196ac8c32
chansol <lchans0319@gmail.com>
2023-09-18 06:01:51 +0900

    슬라이트 컴포넌트 파일 분리

---
+++ components/main/SlideGroups.tsx
@@ -0,0 +1,75 @@
+import Link from 'next/link';
+
+import { SlideMain } from '@/types/main';
+import { news } from '@/types/page';
+
+import { getPath } from '@/utils/page';
+
+import ImageWithFallback from '../common/ImageWithFallback';
+
+interface SlideGroupsProps {
+  currentIndex: number;
+  slideGroups: SlideMain[][];
+}
+
+export default function SlideGroups({ currentIndex, slideGroups }: SlideGroupsProps) {
+  return (
+    <div className="relative">
+      {slideGroups.map((slides, i) => (
+        <SlideGroup key={i} slides={slides} show={i === currentIndex} />
+      ))}
+    </div>
+  );
+}
+
+interface SlideGroupProps {
+  slides: SlideMain[];
+  show?: boolean;
+}
+
+function SlideGroup({ slides, show }: SlideGroupProps) {
+  return (
+    <div
+      className={`flex gap-9 ${
+        show ? 'opacity-100' : 'absolute top-0 opacity-0'
+      } transition-opacity duration-700`}
+    >
+      {slides.map((slide) => (
+        <Slide slide={slide} key={slide.id} />
+      ))}
+    </div>
+  );
+}
+
+interface SlideProps {
+  slide: SlideMain;
+}
+
+const newsPath = getPath(news);
+
+function Slide({ slide }: SlideProps) {
+  return (
+    <article
+      className="w-[185px]"
+      style={{ clipPath: 'polygon(calc(100% - 30px) 0, 100% 30px, 100% 100%, 0 100%, 0 0)' }}
+    >
+      <Link href={`${newsPath}/${slide.id}`} className="block w-[185px] h-[110px] relative">
+        <ImageWithFallback src={slide.imageURL} alt={`${slide.title}_이미지`} fill />
+      </Link>
+      <div className="flex flex-col items-end p-2.5 h-[128px] text-neutral-900 border-x border-b border-[#000]">
+        <h5 className="w-full font-noto mb-1.5 font-bold text-xs line-clamp-2">
+          <Link href={`${newsPath}/${slide.id}`}>{slide.title}</Link>
+        </h5>
+        <p className="w-full font-noto font-light text-[10px] h-11 line-clamp-3">
+          {slide.description}
+        </p>
+        <Link
+          href={`${newsPath}/${slide.id}`}
+          className="grow flex items-end font-noto font-light text-[9px] underline"
+        >
+          더보기
+        </Link>
+      </div>
+    </article>
+  );
+}
--- components/main/SlideScreen.tsx
+++ components/main/SlideScreen.tsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 
 import { SlideMain } from '@/types/main';
 
-import Slide from './Slide';
+import Indicator from './Indicator';
+import SlideGroups from './SlideGroups';
 
 interface SlideScreenProps {
   slides: SlideMain[];
@@ -28,49 +29,11 @@ export default function SlideScreen({ slides }: SlideScreenProps) {
     <div className="flex flex-col gap-5 items-center">
       <div className="flex items-center">
         <DoubleArrowButton direction="left" onClick={movePrev} />
-        <div className="relative">
-          {result.map((items, i) => (
-            <SlideGroup key={i} slides={items} show={i === currIndex} />
-          ))}
-        </div>
+        <SlideGroups currentIndex={currIndex} slideGroups={result} />
         <DoubleArrowButton direction="right" onClick={moveNext} />
       </div>
       <Indicator total={result.length} currentIndex={currIndex} changeIndex={setCurrIndex} />
     </div>
-  );
-}
-
-interface IndicatorProps {
-  total: number;
-  currentIndex: number;
-  changeIndex: (index: number) => void;
-}
-
-function Indicator({ total, currentIndex, changeIndex }: IndicatorProps) {
-  return (
-    <div className="flex gap-2">
-      {Array(total)
-        .fill(0)
-        .map((v, i) => i)
-        .map((index) => (
-          <Dot key={index} index={index} fill={index === currentIndex} changeIndex={changeIndex} />
-        ))}
-    </div>
-  );
-}
-
-interface DotProps {
-  index: number;
-  fill: boolean;
-  changeIndex: (index: number) => void;
-}
-
-function Dot({ index, fill, changeIndex }: DotProps) {
-  return (
-    <button
-      className={`w-2.5 h-2.5 rounded-full ${fill ? 'bg-main-orange' : 'bg-neutral-300'}`}
-      onClick={() => changeIndex(index)}
-    />
   );
 }
 
@@ -86,25 +49,6 @@ function DoubleArrowButton({ direction, onClick }: DoubleArrowButtonProps) {
         {`keyboard_double_arrow_${direction}`}
       </span>
     </button>
-  );
-}
-
-interface SlideGroupProps {
-  slides: SlideMain[];
-  show?: boolean;
-}
-
-function SlideGroup({ slides, show }: SlideGroupProps) {
-  return (
-    <div
-      className={`flex gap-9 ${
-        show ? 'opacity-100' : 'absolute top-0 opacity-0'
-      } transition-opacity duration-700`}
-    >
-      {slides.map((slide) => (
-        <Slide slide={slide} key={slide.id} />
-      ))}
-    </div>
   );
 }
 
