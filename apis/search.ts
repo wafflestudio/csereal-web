33aa89fd9818f342aca44f2e15cbc6e330fe9d1f
Yeolyi <yeolyi1310@gmail.com>
2023-09-18 00:09:36 +0900

    세미나 검색결과 row 추가

--- apis/search.ts
+++ apis/search.ts
@@ -1,7 +1,43 @@
 import { getRequest } from '.';
 
-export const getNoticeSearch = (params: { keyword: string; number: number }) =>
-  getRequest('/notice/totalSearch', params);
+export const getNoticeSearch = async (params: {
+  keyword: string;
+  number: number;
+}): Promise<NoticeSearchResult> => ({
+  total: 3,
+  results: [
+    {
+      id: 1,
+      title: 'TITLE',
+      createdAt: new Date().toISOString(),
+      partialDescription: '12345678912345 789',
+      boldStartIndex: 3,
+      boldEndIndex: 7,
+    },
+  ],
+});
 
-export const getNewsSearch = (params: { keyword: string; number: number }) =>
-  getRequest('/news/totalSearch', params);
+export const getNewsSearch = async (params: {
+  keyword: string;
+  number: number;
+}): Promise<NewsSearchResult> => ({
+  total: 3,
+  results: [
+    {
+      id: 1,
+      title: 'TITLE',
+      date: new Date().toISOString(),
+      partialDescription: '12345678912345 789',
+      boldStartIndex: 3,
+      boldEndIndex: 7,
+      tags: ['TAG1', 'TAG2'],
+      imageUrl: 'asd',
+    },
+  ],
+});
+
+// export const getNoticeSearch = (params: { keyword: string; number: number }) =>
+//   getRequest('/notice/totalSearch', params);
+
+// export const getNewsSearch = (params: { keyword: string; number: number }) =>
+//   getRequest('/news/totalSearch', params);
--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -3,9 +3,13 @@ export const dynamic = 'force-dynamic';
 import { useTranslations } from 'next-intl';
 import Link from 'next-intl/link';
 
+import { getNoticeSearch } from '@/apis/search';
+import { getSeminarPosts } from '@/apis/seminar';
+
 import { CurvedHorizontalNode, StraightNode } from '@/components/common/Nodes';
 import SearchForm from '@/components/search/SearchForm';
 import SearchSubNav from '@/components/search/SearchSubNav';
+import SeminarRow from '@/components/seminar/SeminarRow';
 
 import { news, notice, seminar } from '@/types/page';
 
@@ -21,24 +25,55 @@ interface SearchPageProps {
   };
 }
 
-export default function SearchPage({ searchParams: { query } }: SearchPageProps) {
-  const t = useTranslations('Nav');
+// TODO: 섹션, 서브섹션 일반화하기
+export default async function SearchPage({ searchParams: { query } }: SearchPageProps) {
+  const noticeSearchResult = await getNoticeSearch({ keyword: query, number: 2 });
+  const seminarSearchResult = await getSeminarPosts({ keyword: query, pageNum: 1 });
 
   return (
     <div className="grid grid-rows-[auto_1fr] grid-cols-auto mx-[3.75rem] gap-x-10 justify-center">
       <PageHeader query={query} />
       <div className="flex flex-col w-[52.5rem] row-start-2 col-start-1">
-        <SectionTitle title={t('소식')} size={100} />
+        <SectionTitle title="소식" size={100} />
 
-        <SectionSubtitle title={t('공지사항')} size={1} />
+        <SectionSubtitle title="공지사항" size={noticeSearchResult.total} />
+        <div className="flex flex-col gap-6">
+          {noticeSearchResult.results.slice(0, 2).map((notice) => (
+            <NoticeRow
+              key={notice.id}
+              title={notice.title}
+              description={{
+                content: notice.partialDescription,
+                boldStartIndex: notice.boldStartIndex,
+                boldEndIndex: notice.boldEndIndex,
+              }}
+              dateStr={notice.createdAt}
+            />
+          ))}
+        </div>
         <MoreResultLink href={`${noticePath}?keyword=${query}`} />
         <Divider />
 
-        <SectionSubtitle title={t('새 소식')} size={1} />
+        <SectionSubtitle title="새 소식" size={1} />
         <MoreResultLink href={`${newsPath}?keyword=${query}`} />
         <Divider />
 
-        <SectionSubtitle title={t('세미나')} size={1} />
+        <SectionSubtitle title="세미나" size={seminarSearchResult.total} />
+        <div className="flex flex-col gap-6">
+          {seminarSearchResult.searchList.slice(0, 2).map((seminar) => (
+            <SeminarRow
+              key={seminar.id}
+              id={seminar.id}
+              title={seminar.title}
+              host={seminar.name}
+              company={seminar.affiliation}
+              date={new Date(seminar.startDate)}
+              location={seminar.location}
+              imageURL={seminar.imageURL}
+              isYearLast={seminar.isYearLast}
+            />
+          ))}
+        </div>
         <MoreResultLink href={`${seminarPath}?keyword=${query}`} />
         <Divider />
       </div>
@@ -69,10 +104,11 @@ const PageHeader = ({ query }: { query: string }) => {
 };
 
 const SectionTitle = ({ title, size }: { title: string; size: number }) => {
+  const t = useTranslations('Nav');
   return (
     <div className="mt-12 flex">
       <p className="font-noto border-b border-neutral-300 inline pb-[.59rem] text-[1.25rem] font-bold text-neutral-700 leading-loose">
-        {title}({size})
+        {t(title)}({size})
       </p>
       <div className="flex self-end h-5">
         <div className="self-end border-b border-neutral-300 rotate-[-45deg] origin-bottom-left w-[1.7rem]" />
@@ -83,23 +119,57 @@ const SectionTitle = ({ title, size }: { title: string; size: number }) => {
 };
 
 const SectionSubtitle = ({ title, size }: { title: string; size: number }) => {
+  const t = useTranslations('Nav');
   return (
     <div className="mt-7 flex gap-2 items-center">
       <div className="rounded-full w-[.625rem] h-[.625rem] border border-neutral-500" />
       <h3 className="text-neutral-700 font-noto text-base font-bold leading-loose">
-        {title}({size})
+        {t(title)}({size})
       </h3>
     </div>
   );
 };
 
-const MoreResultLink = ({ href }: { href: string }) => (
-  <Link
-    href={href}
-    className="text-main-orange font-noto text-xs text-middle flex items-center self-end"
-  >
-    결과 더보기<span className="material-symbols-outlined text-sm">chevron_right</span>
-  </Link>
-);
+const MoreResultLink = ({ href }: { href: string }) => {
+  const t = useTranslations('Nav');
+  return (
+    <Link
+      href={href}
+      className="text-main-orange font-noto text-xs text-middle flex items-center self-end"
+    >
+      {t('결과 더보기')}
+      <span className="material-symbols-outlined text-sm">chevron_right</span>
+    </Link>
+  );
+};
 
 const Divider = () => <div className="border-b border-neutral-300 mt-7" />;
+
+interface NoticeRowProps {
+  title: string;
+  description: {
+    content: string;
+    boldStartIndex: number;
+    boldEndIndex: number;
+  };
+  dateStr: string;
+}
+
+const NoticeRow = ({ title, description, dateStr }: NoticeRowProps) => {
+  const date = new Date(dateStr);
+  return (
+    <div className="flex flex-col gap-[.69rem]">
+      <h3>{title}</h3>
+      <p>
+        {description.content.slice(0, description.boldStartIndex)}
+        <span className="font-bold">
+          {description.content.slice(description.boldStartIndex, description.boldEndIndex)}
+        </span>
+        {description.content.slice(description.boldEndIndex)}
+      </p>
+      <time>
+        {date.getFullYear()}/{date.getMonth() + 1}/{date.getDate()}
+      </time>
+    </div>
+  );
+};
