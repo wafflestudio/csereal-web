51daea4e1b47a36387d7116fd89442dfd5530cab
Yeolyi <yeolyi1310@gmail.com>
2023-09-17 21:45:45 +0900

    통합검색 searchForm 적용

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,3 +1,6 @@
+import { CurvedHorizontalNode } from '@/components/common/Nodes';
+import SearchForm from '@/components/search/SearchForm';
+
 interface SearchPageProps {
   searchParams: {
     query: string;
@@ -5,6 +8,26 @@ interface SearchPageProps {
 }
 
 export default function SearchPage({ searchParams }: SearchPageProps) {
-  console.log(searchParams);
-  return <>asd</>;
+  return (
+    <div className="grid grid-rows-[auto_1fr] grid-cols-auto mx-[3.75rem] gap-x-10 justify-center">
+      <PageTitle />
+      <SearchForm />
+      <div className="w-[52.5rem] row-start-2 col-start-1"></div>
+    </div>
+  );
 }
+
+const PageTitle = () => {
+  return (
+    <div className={`w-fit min-w-[15.625rem] max-w-[51.875rem] row-start-1 col-start-1 mt-1`}>
+      <div className="flex gap-2 mb-[1.19rem]">
+        <CurvedHorizontalNode grow={true} />
+      </div>
+      <h3
+        className={`mr-[65px] break-keep font-yoon text-neutral-800 tracking-wide text-2xl font-bold`}
+      >
+        통합 검색
+      </h3>
+    </div>
+  );
+};
