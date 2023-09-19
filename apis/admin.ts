7f4900b1ff8fe446264344a0aaa0179fcbb33544
chansol <lchans0319@gmail.com>
2023-09-20 01:43:31 +0900

    관리자 페이지 데이터 임시로 없앰

--- apis/admin.ts
+++ apis/admin.ts
@@ -27,17 +27,11 @@ export const patchMultipleImportants = (targetInfos: { id: number; category: str
   });
 
 export const getSlides = async (): Promise<{ posts: SlidePreview[]; total: number }> => ({
-  posts: [
-    { title: '슬라이드', id: 1, createdAt: '2023-08-05' },
-    { title: '슬라이드', id: 2, createdAt: '2023-08-05' },
-  ],
-  total: 2,
+  posts: [],
+  total: 0,
 });
 
 export const getImportants = async (): Promise<{ posts: ImportantPreview[]; total: number }> => ({
-  posts: [
-    { title: '슬라이드', id: 1, createdAt: '2023-08-05', category: 'notice' },
-    { title: '슬라이드', id: 2, createdAt: '2023-08-05', category: 'news' },
-  ],
-  total: 2,
+  posts: [],
+  total: 0,
 });
