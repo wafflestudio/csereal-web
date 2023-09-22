d065a00f5981ac41935a2fe0a99b4db804fb3bc2
chansol <lchans0319@gmail.com>
2023-09-22 22:27:53 +0900

    관리자 api 주석 해제

--- apis/adminServer.ts
+++ apis/adminServer.ts
@@ -2,43 +2,43 @@ import { ImportantPreview, SlidePreview } from '@/types/admin';
 
 import { getRequest, patchRequest } from './serverIndex';
 
-// export const getSlides = (pageNum: number) =>
-//   getRequest(
-//     '/admin/slide',
-//     { pageNum },
-//     {
-//       next: { tags: ['slide'] },
-//     },
-//   ) as Promise<{
-//     slides: SlidePreview[];
-//     total: number;
-//   }>;
+export const getSlides = (pageNum: number) =>
+  getRequest(
+    '/admin/slide',
+    { pageNum },
+    {
+      next: { tags: ['slide'] },
+    },
+  ) as Promise<{
+    slides: SlidePreview[];
+    total: number;
+  }>;
 
-// export const getImportants = (pageNum: number) =>
-//   getRequest(
-//     '/admin/important',
-//     { pageNum },
-//     {
-//       next: { tags: ['important'] },
-//     },
-//   ) as Promise<{
-//     importants: ImportantPreview[];
-//     total: number;
-//   }>;
+export const getImportants = (pageNum: number) =>
+  getRequest(
+    '/admin/important',
+    { pageNum },
+    {
+      next: { tags: ['important'] },
+    },
+  ) as Promise<{
+    importants: ImportantPreview[];
+    total: number;
+  }>;
 
-export const getSlides = async (
-  page: number,
-): Promise<{ slides: SlidePreview[]; total: number }> => ({
-  slides: [],
-  total: 0,
-});
+// export const getSlides = async (
+//   page: number,
+// ): Promise<{ slides: SlidePreview[]; total: number }> => ({
+//   slides: [],
+//   total: 0,
+// });
 
-export const getImportants = async (
-  page: number,
-): Promise<{ importants: ImportantPreview[]; total: number }> => ({
-  importants: [],
-  total: 0,
-});
+// export const getImportants = async (
+//   page: number,
+// ): Promise<{ importants: ImportantPreview[]; total: number }> => ({
+//   importants: [],
+//   total: 0,
+// });
 
 export const patchMultipleSlides = (newsIdList: number[]) =>
   patchRequest('/admin/slide', {
