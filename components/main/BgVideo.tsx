55ccb3367440571d08e4d2af2bb58a322d86db25
chansol <lchans0319@gmail.com>
2023-09-26 19:33:37 +0900

    메인 mp4 -> gif

--- components/main/BgVideo.tsx
+++ components/main/BgVideo.tsx
@@ -1,14 +1,19 @@
+import Image from 'next/image';
+
+import mainBg from '@/public/image/main_bg.gif';
+
 export default function BgVideo() {
   return (
     <section className="absolute flex w-full justify-center top-[-60px]">
-      <video
+      <Image src={mainBg} alt="main_gif" width={1200} />
+      {/* <video
         id="background-video"
         loop
         autoPlay
         muted
         className="w-[1200px]"
         src={'video/main_video_bg.mp4'}
-      />
+      /> */}
     </section>
   );
 }
