1ff30c51d4ae154463d009cb3eef248d3526db26
chansol <lchans0319@gmail.com>
2023-09-26 23:43:15 +0900

    리스트 정렬 기준 추가

--- components/academics/CourseList.tsx
+++ components/academics/CourseList.tsx
@@ -1,4 +1,4 @@
-import { Course, SortOption } from '@/types/academics';
+import { Classification, Course, SortOption } from '@/types/academics';
 
 import CourseListHeader from './CourseListHeader';
 import CourseListRow from './CourseListRow';
@@ -9,14 +9,39 @@ interface CourseListProps {
 }
 
 export default function CourseList({ courses, selectedOption }: CourseListProps) {
+  const sortedCourses = sortCourses(courses ?? [], selectedOption);
+
   return (
     <div className="border-b border-neutral-200">
       <CourseListHeader />
       <ul className="divide-y divide-neutral-200 divide-dashed font-noto ">
-        {courses.map((course) => (
+        {sortedCourses.map((course) => (
           <CourseListRow course={course} key={course.code} />
         ))}
       </ul>
     </div>
   );
 }
+
+const ClassificationPriority: { [key in Classification]: number } = {
+  전공필수: 1,
+  전공선택: 2,
+  교양: 3,
+};
+
+const sortCourses = (courses: Course[], sortOption: SortOption) => {
+  if (sortOption === '학년') {
+    courses.sort((c1, c2) => {
+      if (c1.grade === '대학원' || c2.grade === '대학원') return 0;
+      return parseInt(c1.grade) - parseInt(c2.grade);
+    });
+  } else if (sortOption === '교과목 구분') {
+    courses.sort((c1, c2) => {
+      return ClassificationPriority[c1.classification] - ClassificationPriority[c2.classification];
+    });
+  } else {
+    courses.sort((c1, c2) => c1.credit - c2.credit);
+  }
+
+  return courses;
+};
