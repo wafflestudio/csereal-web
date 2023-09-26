11a6cc787bdb55a0af265aa515e379ac1d8b9706
chansol <lchans0319@gmail.com>
2023-09-26 19:43:00 +0900

    비디오 파일 삭제

--- components/main/BgVideo.tsx
+++ components/main/BgVideo.tsx
@@ -5,15 +5,7 @@ import mainBg from '@/public/image/main_bg.gif';
 export default function BgVideo() {
   return (
     <section className="absolute flex w-full justify-center top-[-60px]">
-      <Image src={mainBg} alt="main_gif" width={1200} />
-      {/* <video
-        id="background-video"
-        loop
-        autoPlay
-        muted
-        className="w-[1200px]"
-        src={'video/main_video_bg.mp4'}
-      /> */}
+      <Image src={mainBg} alt="main_gif" width={1200} priority />
     </section>
   );
 }
