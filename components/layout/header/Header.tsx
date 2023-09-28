81ef904b3632b6f2a21cf2822c5e41a45d60d853
Limchansol <lchans0319@gmail.com>
2023-09-29 01:40:33 +0900

    💄 헤더 pb 2rem (#114)

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
   return (
     <header
       className={`
-         top-0 bg-white px-[3.75rem] pt-[40px] pb-[1rem] flex w-full justify-between gap-4 z-40
+         top-0 bg-white px-[3.75rem] pt-[40px] pb-8 flex w-full justify-between gap-4 z-40
       `}
     >
       <div onClick={refreshPage} className="cursor-pointer flex-1">
