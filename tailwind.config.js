ab26e1626214879bb914e5b093cf83f857e425ae
Yeolyi <yeolyi1310@gmail.com>
2024-01-06 13:17:29 +0900

    lightmode 임시 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/.internal/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {
-      neutral: {
-        50: '#121212',
-        100: '#171717',
-        150: '#1e1e1e',
-        200: '#262626',
-        300: '#404040',
-        400: '#737373',
-        500: '#a3a3a3',
-        600: '#d4d4d4',
-        700: '#e5e5e5',
-        800: '#fafafa',
-        900: '#ffffff',
-      },
+      neutral: colors.neutral,
       white: {
-        DEFAULT: '#141212',
+        DEFAULT: '#ffffff',
       },
       'main-orange': {
-        DEFAULT: '#e9390b',
+        DEFAULT: '#ff6914',
+        dark: '#cf5c10',
       },
       link: {
         DEFAULT: '#3c7be4',
@@ -35,12 +24,10 @@ module.exports = {
         // TBD
         DEFAULT: '#ff0000',
       },
-      transparent: 'transparent',
     },
     fontFamily: {
       yoon: ['var(--font-yoon)'],
       noto: ['var(--font-noto)'],
-      'noto-demi': ['var(--font-noto-demi)'],
     },
     fontSize: {
       xs: '0.75rem', // 12px
@@ -50,7 +37,6 @@ module.exports = {
       lg: '1.125rem', // 18px
       xl: '1.375rem', // 22px changed
       '2xl': '1.5rem', // 24px
-      '3xl': '1.875rem', // 30px
     },
 
     extend: {
@@ -59,14 +45,9 @@ module.exports = {
           '0%': { width: '10px' },
           '100%': { width: '100%' },
         },
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
       },
       animation: {
         stretch: 'stretch 0.7s ease-out',
-        fadeIn: 'fadeIn 2s ease-out',
       },
     },
   },
