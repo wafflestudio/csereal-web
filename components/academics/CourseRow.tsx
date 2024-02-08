a50d383e8f01720839a34b722f5b2e052d366b2c
chansol <lchans0319@gmail.com>
2024-02-09 01:17:09 +0900

    design: 캐러셀 화살표 추가

--- components/academics/CourseRow.tsx
+++ components/academics/CourseRow.tsx
@@ -15,12 +15,24 @@ export default function CourseRow({ courses, selectedOption }: CourseRowProps) {
   const { scrollRef: screenRef } = useHorizontalScroll();
 
   return (
-    <div className="styled-scrollbar overflow-y-hidden overflow-x-auto p-1.5" ref={screenRef}>
-      <div className="flex gap-5">
-        {courses.map((course) => (
-          <CourseCard course={course} selectedOption={selectedOption} key={course.code} />
-        ))}
+    <div className="flex items-center group">
+      <button className="invisible group-hover:visible">
+        <span className="material-symbols-rounded font-light text-[32px] text-main-orange">
+          navigate_before
+        </span>
+      </button>
+      <div className="styled-scrollbar overflow-y-hidden overflow-x-auto py-1.5" ref={screenRef}>
+        <div className="flex gap-5">
+          {courses.map((course) => (
+            <CourseCard course={course} selectedOption={selectedOption} key={course.code} />
+          ))}
+        </div>
       </div>
+      <button className="invisible group-hover:visible">
+        <span className="material-symbols-rounded font-light text-[32px] text-main-orange">
+          navigate_next
+        </span>
+      </button>
     </div>
   );
 }
