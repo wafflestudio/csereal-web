435607623970b60fa6a9a0976889c617f79faed0
Yeolyi <yeolyi1310@gmail.com>
2024-02-08 17:36:23 +0900

    design: Header 디자인 적용

---
+++ hooks/useLanguage.ts
@@ -0,0 +1,21 @@
+'use-client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+export default function useLanguage() {
+  const router = useRouter();
+  const path = usePathname();
+  const searchParams = useSearchParams();
+
+  const isEnglish = path.startsWith('/en');
+
+  const changeLanguage = () => {
+    if (isEnglish) {
+      router.push(`/ko${path.slice(3)}?${searchParams}`);
+    } else {
+      router.push(`/en${path}?${searchParams}`);
+    }
+  };
+
+  return { isEnglish, changeLanguage };
+}
