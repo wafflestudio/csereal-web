c0bd4b558ef89366191563b2a7558112d29df121
Limchansol <lchans0319@gmail.com>
2024-03-01 15:33:10 +0900

    ✨ 대분류 페이지 2차 추가 (#140)
    * feat: 다차원 대분류 추가
    
    * fix: 2차원 여부 표시
    
    * feat: 입학 대분류 페이지 추가
    
    * feat: 시설 예약 대분류 페이지 추가
    
    * feat: 대분류 아이템 영어 이름 추가
    
    * design: 대분류 제목 패딩 조정
    
    * design: 헤더 색 투명하게 변경

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,9 +1,5 @@
-import { getAboutDescription } from '@/apis/about';
-
 import MajorCategoryPageLayout from '@/components/layout/pageLayout/MajorCategoryPageLayout';
 
 export default async function AboutPage() {
-  const description = await getAboutDescription();
-
-  return <MajorCategoryPageLayout subtitle="About CSE" description={description} />;
+  return <MajorCategoryPageLayout subtitle="About CSE" />;
 }
---
+++ app/[locale]/reservations/page.tsx
@@ -0,0 +1,5 @@
+import MajorCategoryPageLayout from '@/components/layout/pageLayout/MajorCategoryPageLayout';
+
+export default async function ReservationsPage() {
+  return <MajorCategoryPageLayout subtitle="Reserve CSE" twoDimensional />;
+}
