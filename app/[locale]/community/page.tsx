7015d91c9c65668accc466ea20ddd1901644cfcf
chansol <lchans0319@gmail.com>
2024-03-01 15:59:55 +0900

    feat: 대분류 페이지 반응형

--- app/[locale]/community/page.tsx
+++ app/[locale]/community/page.tsx
@@ -1,9 +1,5 @@
-import { getCommunityDescription } from '@/apis/community';
-
 import MajorCategoryPageLayout from '@/components/layout/pageLayout/MajorCategoryPageLayout';
 
 export default async function CommunityPage() {
-  const description = await getCommunityDescription();
-
-  return <MajorCategoryPageLayout subtitle="Communicate with CSE" description={description} />;
+  return <MajorCategoryPageLayout subtitle="Communicate with CSE" />;
 }
