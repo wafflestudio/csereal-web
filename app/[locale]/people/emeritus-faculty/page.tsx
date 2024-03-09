c19e83bcdd96d073033b82b051dacff49199b892
chansol <lchans0319@gmail.com>
2024-03-10 02:21:27 +0900

    fix: 명예교수진 반응형 추가

--- app/[locale]/people/emeritus-faculty/page.tsx
+++ app/[locale]/people/emeritus-faculty/page.tsx
@@ -2,28 +2,16 @@ export const dynamic = 'force-dynamic';
 
 import { getEmeritusFacultyList } from '@/apis/people';
 
-import PeopleCell from '@/app/[locale]/people/PeopleRow';
-
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
+
+import PeopleGrid from '../PeopleGrid';
 
 export default async function EmeritusFacultyPage() {
   const facultyList = await getEmeritusFacultyList();
 
   return (
     <PageLayout title="역대 교수진" titleType="big">
-      <div className="grid grid-cols-[repeat(auto-fit,_144px)] gap-14">
-        {facultyList.map((emeritusFaculty, index) => (
-          <PeopleCell
-            type="EMIRITUS_FACULTY"
-            key={index}
-            id={emeritusFaculty.id}
-            name={emeritusFaculty.name}
-            academicRank={emeritusFaculty.academicRank}
-            email={emeritusFaculty.email}
-            imageURL={emeritusFaculty.imageURL}
-          />
-        ))}
-      </div>
+      <PeopleGrid people={facultyList} peopleType="EMIRITUS_FACULTY" />
     </PageLayout>
   );
 }
