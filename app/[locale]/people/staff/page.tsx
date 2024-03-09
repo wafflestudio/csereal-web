1c32b0af19e32f3e9fb3a3c9f041fbbf9df83305
chansol <lchans0319@gmail.com>
2024-03-10 02:20:22 +0900

    fix: 행정직원 반응형 추가

--- app/[locale]/people/staff/page.tsx
+++ app/[locale]/people/staff/page.tsx
@@ -2,30 +2,16 @@ export const dynamic = 'force-dynamic';
 
 import { getStaffList } from '@/apis/people';
 
-import PeopleCell from '@/app/[locale]/people/PeopleRow';
-
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
+
+import PeopleGrid from '../PeopleGrid';
 
 export default async function StaffPage() {
   const staffList = await getStaffList();
 
   return (
     <PageLayout title="행정직원" titleType="big">
-      <div className="grid grid-cols-[repeat(auto-fit,_144px)] gap-14">
-        {staffList.map((staff, index) => (
-          <PeopleCell
-            type="STAFF"
-            key={index}
-            id={staff.id}
-            name={staff.name}
-            role={staff.role}
-            office={staff.office}
-            email={staff.email}
-            phone={staff.phone}
-            imageURL={staff.imageURL}
-          />
-        ))}
-      </div>
+      <PeopleGrid people={staffList} peopleType="STAFF" />
     </PageLayout>
   );
 }
