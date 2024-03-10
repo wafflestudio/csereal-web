60b2833f529815d7eaea9ac517a49ad756329b8e
chansol <lchans0319@gmail.com>
2024-03-10 19:26:54 +0900

    fix: resize 발생 전에는 모바일 감지가 제대로 되지 않던 에러 수정

--- utils/hooks/useResponsive.ts
+++ utils/hooks/useResponsive.ts
@@ -10,6 +10,7 @@ export default function useResponsive() {
   const [screenType, setScreenType] = useState<ScreenType>('desktop');
 
   const handleResize = useCallback(() => {
+    console.log('Adfasdf');
     if (window.innerWidth < BREAK_POINT.sm) {
       setScreenType('mobile');
     } else if (window.innerWidth >= BREAK_POINT.sm) {
@@ -18,6 +19,7 @@ export default function useResponsive() {
   }, []);
 
   useEffect(() => {
+    handleResize(); // resize 이벤트가 발생하지 않는 맨 처음 현재 환경을 파악하기 위해 실행
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, [handleResize]);
