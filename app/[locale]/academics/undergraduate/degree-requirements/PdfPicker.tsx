0f1b200fbaa329be9e6256abe69e7fa53cbdcc1f
Yeolyi <yeolyi1310@gmail.com>
2024-03-10 17:48:44 +0900

    refactor: 학부 졸업 규정 SSR로 수정

---
+++ app/[locale]/academics/undergraduate/degree-requirements/PdfPicker.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
+import Attachments from '@/components/common/Attachments';
+import Dropdown from '@/components/common/form/Dropdown';
+
+import { DegreeRequirements } from '@/types/academics';
+
+export default function PdfPicker({ data }: { data: DegreeRequirements[] }) {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row items-center gap-2">
+        <p className="text-sm">입학연도</p>
+        <Dropdown
+          contents={data.map((item) => item.year.toString())}
+          selectedIndex={index}
+          onClick={setIndex}
+        />
+      </div>
+      <Attachments files={[data[index].attachment]} />
+    </div>
+  );
+}
