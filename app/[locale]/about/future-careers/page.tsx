f75d1a4d955f5f207c8f25107e433cb87eddec19
chansol <lchans0319@gmail.com>
2024-03-11 17:31:52 +0900

    fix: 졸업생 창업 기업 표 웹 반응형

--- app/[locale]/about/future-careers/page.tsx
+++ app/[locale]/about/future-careers/page.tsx
@@ -22,9 +22,9 @@ export default async function GreetingsPage() {
 
 function CareerCompanies({ companies }: { companies: FutureCareers['companies'] }) {
   return (
-    <div>
-      <h3 className="mb-[0.8rem] text-base font-bold leading-[1.625rem]">졸업생 창업 기업</h3>
-      <div className="border-y border-neutral-200 text-sm font-normal sm:inline-block">
+    <div className="sm:max-w-[780px]">
+      <h3 className="mb-3 text-base font-bold">졸업생 창업 기업</h3>
+      <div className="border-y border-neutral-200 text-sm font-normal">
         <CompanyTableHeader />
         <ol>
           {companies.map((company, index) => (
@@ -40,11 +40,11 @@ const TABLE_COLUMN_SIZE = ['sm:w-[3.5rem]', 'sm:w-[12.5rem]', 'sm:w-80', 'sm:w-2
 
 function CompanyTableHeader() {
   return (
-    <div className="hidden h-10 items-center border-b border-neutral-200 sm:flex [&_p]:pl-3">
+    <div className="hidden h-10 items-center gap-3 whitespace-nowrap border-b border-neutral-200 sm:flex sm:px-3">
       <p className={TABLE_COLUMN_SIZE[0]}>연번</p>
       <p className={TABLE_COLUMN_SIZE[1]}>창업 기업명</p>
       <p className={TABLE_COLUMN_SIZE[2]}>홈페이지</p>
-      <p className={TABLE_COLUMN_SIZE[3]}>창업연도</p>
+      <p className={TABLE_COLUMN_SIZE[3] + ''}>창업연도</p>
     </div>
   );
 }
@@ -58,26 +58,22 @@ interface CompanyTableRowProps {
 
 function CompanyTableRow({ index, name, url, year }: CompanyTableRowProps) {
   return (
-    <li className="grid grid-cols-[22px,_auto,_1fr] items-center gap-x-1 px-7 py-6 odd:bg-neutral-100 sm:flex sm:h-10 sm:gap-0 sm:p-0">
+    <li className="grid grid-cols-[22px,_auto,_1fr] items-center gap-x-1 px-7 py-6 odd:bg-neutral-100 sm:flex sm:h-10 sm:gap-3 sm:p-0 sm:px-3">
       <p className={`text-sm text-neutral-400 sm:pl-5 ${TABLE_COLUMN_SIZE[0]}`}>{index}</p>
-      <p
-        className={`text-md font-medium sm:pl-3 sm:text-sm sm:font-normal ${TABLE_COLUMN_SIZE[1]}`}
-      >
+      <p className={`text-md font-medium sm:text-sm sm:font-normal ${TABLE_COLUMN_SIZE[1]}`}>
         {name}
       </p>
       <a
         className={`order-last col-span-2 col-start-2 ${
           url && 'mt-1'
-        } w-fit text-xs text-link hover:underline sm:order-none sm:mt-0 sm:pl-3 ${
-          TABLE_COLUMN_SIZE[2]
-        }
+        } w-fit text-xs text-link hover:underline sm:order-none sm:mt-0 ${TABLE_COLUMN_SIZE[2]}
         `}
         href={url}
         target="_blank"
       >
         {url}
       </a>
-      <p className={`pl-1 text-sm text-neutral-400 sm:pl-3.5 ${TABLE_COLUMN_SIZE[3]}`}>{year}</p>
+      <p className={`pl-1 text-sm text-neutral-400 ${TABLE_COLUMN_SIZE[3]}`}>{year}</p>
     </li>
   );
 }
