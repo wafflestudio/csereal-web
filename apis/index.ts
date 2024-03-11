10ef08b0d97de4326e59944704c6e6011848401b
Yeolyi <yeolyi1310@gmail.com>
2024-03-11 14:30:17 +0900

    feat: fetchWithRetry 구현

--- apis/index.ts
+++ apis/index.ts
@@ -4,18 +4,20 @@ import { objToQueryString } from '@/utils/convertParams';
 
 export const BASE_URL = 'https://cse-dev-waffle.bacchus.io/api/v1';
 
+// REST request
+
 export const getRequest = async <T = unknown>(
   url: string,
   params: object = {},
   init?: RequestInit,
 ): Promise<T> => {
   const queryString = objToQueryString(params);
-  const resp = await fetchWithCredentials(`${BASE_URL}${url}${queryString}`, 'GET', init);
+  const resp = await fetchWithRetry(`${BASE_URL}${url}${queryString}`, 'GET', init);
   return await resp.json();
 };
 
 export const postRequest = async <T = unknown>(url: string, init?: RequestInit): Promise<T> => {
-  const resp = await fetchWithCredentials(`${BASE_URL}${url}`, 'POST', init);
+  const resp = await fetchWithRetry(`${BASE_URL}${url}`, 'POST', init);
   return await resp.json();
 };
 
@@ -23,12 +25,30 @@ export const patchRequest = async <T = unknown>(
   url: string,
   init?: RequestInit,
 ): Promise<T | null> => {
-  const resp = await fetchWithCredentials(`${BASE_URL}${url}`, 'PATCH', init);
+  const resp = await fetchWithRetry(`${BASE_URL}${url}`, 'PATCH', init);
   return resp.headers.get('content-type') ? await resp.json() : null;
 };
 
 export const deleteRequest = async (url: string, init?: RequestInit) => {
-  await fetchWithCredentials(`${BASE_URL}${url}`, 'DELETE', init);
+  await fetchWithRetry(`${BASE_URL}${url}`, 'DELETE', init);
+};
+
+// util
+
+/**
+ * 학교 서버가 간헐적으로 첫 요청에서 ECONNRESET이 뜨는 경우가 있어 한 번 더 요청.
+ * 안전을 위해 멱등성이 있는 GET 요청에 대해서만 retry
+ */
+const fetchWithRetry = async (url: string, method: string, init?: RequestInit) => {
+  if (method !== 'GET') return fetchWithCredentials(url, method, init);
+
+  try {
+    return await fetchWithCredentials(url, method, init);
+  } catch (e) {
+    if (e instanceof NetworkError) throw e;
+    console.log('fetchWithRetry: 서버 에러 감지');
+    return fetchWithCredentials(url, method, init);
+  }
 };
 
 const fetchWithCredentials = async (url: string, method: string, init?: RequestInit) => {
@@ -48,7 +68,6 @@ const fetchWithCredentials = async (url: string, method: string, init?: RequestI
 
 export const checkError = (response: Response) => {
   if (response.ok) return;
-
   throw new NetworkError(response.status);
 };
 
