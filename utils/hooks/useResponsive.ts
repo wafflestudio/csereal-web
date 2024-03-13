b3c7c303f74896ca49f7d84cba762ba6a9ff0f20
Yeolyi <yeolyi1310@gmail.com>
2024-03-13 12:07:07 +0900

    Revert "refactor/useResponsive에서 useMediaQuery 사용"
    This reverts commit efb6631c271d2c5ab8824eb6650dad0cf29a319c.

--- utils/hooks/useResponsive.ts
+++ utils/hooks/useResponsive.ts
@@ -1,6 +1,27 @@
-import { useMediaQuery } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
+
+const BREAK_POINT = {
+  sm: 640 /* mobile min-width: 640px */,
+};
+
+type ScreenType = 'mobile' | 'desktop';
 
 export default function useResponsive() {
-  const matches = useMediaQuery('(min-width: 640px');
-  return { screenType: matches ? 'desktop' : 'mobile' };
+  const [screenType, setScreenType] = useState<ScreenType>('desktop');
+
+  const handleResize = useCallback(() => {
+    if (window.innerWidth < BREAK_POINT.sm) {
+      setScreenType('mobile');
+    } else if (window.innerWidth >= BREAK_POINT.sm) {
+      setScreenType('desktop');
+    }
+  }, []);
+
+  useEffect(() => {
+    handleResize(); // resize 이벤트가 발생하지 않는 맨 처음 현재 환경을 파악하기 위해 실행
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [handleResize]);
+
+  return { screenType };
 }
