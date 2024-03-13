6ac9f454f9aed4212cefe99447c6881aa6c8e2e9
Yeolyi <yeolyi1310@gmail.com>
2024-03-13 23:52:01 +0900

    feat: layout.tsx 수정

--- app/[locale]/MarginedMain.tsx
+++ app/[locale]/MarginedMain.tsx
@@ -7,9 +7,9 @@ import { main as mainNode } from '@/utils/segmentNode';
 
 export default function MarginedMain({ children }: { children: ReactNode }) {
   const node = useCurrentSegmentNode();
-  const marginLeft = node === mainNode ? `sm:ml-[4.75rem]` : '';
+  const marginLeft = node === mainNode ? `sm:ml-[11rem]` : 'sm:ml-[6.25rem]';
 
   // iOS에서 상단바 액션(scrolltoTop)을 지원하기 위해 모바일에서는 overflow-scroll 제외
   // 데스크톱에서는 좌측 네비를 고정시키기 위해서 필요
-  return <main className={`flex grow flex-col sm:overflow-scroll ${marginLeft}`}>{children}</main>;
+  return <main className={`flex grow flex-col ${marginLeft}`}>{children}</main>;
 }
--- components/layout/navbar/Navbar.tsx
+++ components/layout/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import useCurrentSegmentNode from '@/utils/hooks/useCurrentSegmentNode';
 import { main } from '@/utils/segmentNode';
 
 import NavbarDetail from './NavbarDetail';
-import NavbarRoot, { NAVBAR_CLOSED_WIDTH_REM } from './NavbarRoot';
+import NavbarRoot from './NavbarRoot';
 
 // 네비바 컴포넌트들은 모두 interactivity가 크므로 CSR 처리합니다.
 export default function Navbar() {
@@ -18,14 +18,12 @@ export default function Navbar() {
   };
 
   return (
-    <div className={`sticky bottom-0 top-0 z-50 shrink-0 sm:w-[${NAVBAR_CLOSED_WIDTH_REM}rem]`}>
-      <div
-        className={`absolute bottom-0 left-0 top-0 hidden sm:flex`}
-        onMouseLeave={handleMouseLeave}
-      >
-        <NavbarRoot />
-        <NavbarDetail />
-      </div>
+    <div
+      className={`fixed bottom-0 left-0 top-0 z-50 hidden sm:flex`}
+      onMouseLeave={handleMouseLeave}
+    >
+      <NavbarRoot />
+      <NavbarDetail />
     </div>
   );
 }
