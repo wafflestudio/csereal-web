9e9f1195d24f6094168fb78a8c41f5565fa605e5
이성열 <yeolyi1310@gmail.com>
2024-04-01 12:03:11 +0900

    feat: 404 페이지 추가 (#183)

---
+++ app/[locale]/[...rest]/page.tsx
@@ -0,0 +1,6 @@
+// https://next-intl-docs.vercel.app/docs/environments/error-files
+import { notFound } from 'next/navigation';
+
+export default function CatchAllPage() {
+  notFound();
+}
--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,17 +1,12 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
-
-import { Link } from '@/navigation';
+import Header from '@/components/layout/header/Header';
 
 export default function NotFound() {
-  const path = usePathname();
   return (
-    <div className="mx-[3.75rem] mt-3 flex flex-col items-start">
-      <p className="text-lg">{path}는 존재하지 않는 경로입니다.</p>
-      <Link href="/" className="text-sm text-link underline">
-        메인으로 가기
-      </Link>
-    </div>
+    <>
+      <Header />
+      <div className="grow p-[3.75rem]">
+        <p className="text-lg text-white">존재하지 않는 경로입니다.</p>
+      </div>
+    </>
   );
 }
