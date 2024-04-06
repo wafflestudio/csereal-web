8a8c83aec7331d620c7c50c93cfac10798d56037
chansol <lchans0319@gmail.com>
2024-04-06 17:08:29 +0900

    fix: useResponsive 화면 크기 옵션 추가

--- utils/hooks/useResponsive.ts
+++ utils/hooks/useResponsive.ts
@@ -1,16 +1,29 @@
 import { useMediaQuery } from '@mui/material';
 
+// px
 const BREAK_POINT = {
-  sm: 640 /* mobile min-width: 640px */,
+  sm: 640 /* mobile */,
+  md: 768 /* tablet */,
+  lg: 1024 /* desktop */,
   xl: 1280 /* wide desktop */,
 };
 
-export default function useResponsive(): {
-  isMobile: boolean;
-  isDesktopWide: boolean;
-} {
-  const isMobile = !useMediaQuery(`(min-width: ${BREAK_POINT.sm}px)`);
+/**
+ * mobile: 0px ~ 639px
+ * tablet: 640px ~ 1023px
+ * desktop: 1024px ~ 1279px
+ * desktop wide: 1280px ~ inf.
+ * @returns boolean value variables [is{status}]
+ */
+export default function useResponsive() {
+  const isMobile = useMediaQuery(`(max-width: ${BREAK_POINT.sm - 1}px)`);
+  const isTablet = useMediaQuery(
+    `(min-width: ${BREAK_POINT.md}px) and (max-width:${BREAK_POINT.lg - 1}px)`,
+  );
+  const isDesktop = useMediaQuery(
+    `(min-width: ${BREAK_POINT.lg}px) and (max-width:${BREAK_POINT.xl - 1}px)`,
+  );
   const isDesktopWide = useMediaQuery(`(min-width: ${BREAK_POINT.xl}px)`);
 
-  return { isMobile, isDesktopWide };
+  return { isMobile, isTablet, isDesktop, isDesktopWide };
 }
