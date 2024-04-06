48a3f72778a258d6f81e0ebbf97997f235e35424
chansol <lchans0319@gmail.com>
2024-04-06 18:10:20 +0900

    fix: 역대 교수진 모바일

--- app/[locale]/people/emeritus-faculty/[id]/page.tsx
+++ app/[locale]/people/emeritus-faculty/[id]/page.tsx
@@ -15,36 +15,43 @@ export default async function EmeritusFacultyMemberPage({ params }: { params: { 
 
   return (
     <PageLayout title={<PageTitle {...faculty} />} titleType="big" titleMargin="mb-9">
-      <div className="relative mb-10 flow-root">
-        <ProfileImage imageURL={faculty.imageURL} />
-        {(faculty.office || faculty.email || faculty.website) && (
-          <article className="mb-7 flex flex-col text-neutral-700">
-            <h3 className=" text-base font-bold leading-8">연락처 정보</h3>
-            <ul className="list-inside list-disc">
-              {faculty.office && <BulletRow>교수실: {faculty.office}</BulletRow>}
-              {faculty.email && (
-                <BulletRow>
-                  이메일:
-                  <Link className="ml-1 text-link hover:underline" href={`mailto:${faculty.email}`}>
-                    {faculty.email}
-                  </Link>
-                </BulletRow>
-              )}
-              {faculty.website && (
-                <BulletRow>
-                  웹사이트:
-                  <Link className="ml-1 text-link hover:underline" href={`${faculty.website}`}>
-                    {faculty.website}
-                  </Link>
-                </BulletRow>
-              )}
-            </ul>
-          </article>
-        )}
-        <HeaderAndList header="학력" list={faculty.educations} />
-        <HeaderAndList header="연구 분야" list={faculty.researchAreas ?? []} />
-        <div className=" mb-7 text-sm font-medium text-neutral-700">
-          재직 기간: {careerTime.startTime} - {careerTime.endTime}
+      <div className="relative mb-10 flex flex-col-reverse sm:flex-row sm:gap-10">
+        <div>
+          <HeaderAndList header="학력" list={faculty.educations} />
+          <HeaderAndList header="연구 분야" list={faculty.researchAreas ?? []} />
+          <div className=" mb-7 text-sm font-medium text-neutral-700">
+            재직 기간: {careerTime.startTime} - {careerTime.endTime}
+          </div>
+        </div>
+        <div>
+          <ProfileImage imageURL={faculty.imageURL} />
+          {(faculty.office || faculty.email || faculty.website) && (
+            <article className="mb-7 flex flex-col text-neutral-700">
+              <h3 className=" text-base font-bold leading-8">연락처 정보</h3>
+              <ul className="list-inside list-disc">
+                {faculty.office && <BulletRow>교수실: {faculty.office}</BulletRow>}
+                {faculty.email && (
+                  <BulletRow>
+                    이메일:
+                    <Link
+                      className="ml-1 text-link hover:underline"
+                      href={`mailto:${faculty.email}`}
+                    >
+                      {faculty.email}
+                    </Link>
+                  </BulletRow>
+                )}
+                {faculty.website && (
+                  <BulletRow>
+                    웹사이트:
+                    <Link className="ml-1 text-link hover:underline" href={`${faculty.website}`}>
+                      {faculty.website}
+                    </Link>
+                  </BulletRow>
+                )}
+              </ul>
+            </article>
+          )}
         </div>
       </div>
     </PageLayout>
