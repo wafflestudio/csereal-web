d0d0ce14d74795b392c68061db7821d1cc7c26f9
chansol <lchans0319@gmail.com>
2024-04-06 18:03:06 +0900

    fix: 행정직원 개별 페이지 모바일

--- app/[locale]/people/staff/[id]/page.tsx
+++ app/[locale]/people/staff/[id]/page.tsx
@@ -19,9 +19,10 @@ export default async function StaffMemberPage({ params }: { params: { id: number
       titleType="big"
       titleMargin="mb-9"
     >
-      <div className="relative mb-32 flow-root">
-        <ProfileImage imageURL={staff.imageURL} />
-        <div className="break-all">
+      <div className="relative mb-32 flex flex-col-reverse sm:flex-row sm:gap-8">
+        <HeaderAndList header="주요 업무" list={staff.tasks} />
+        <div>
+          <ProfileImage imageURL={staff.imageURL} />
           <article className="mb-7 flex flex-col text-neutral-700">
             <h3 className=" text-base font-bold leading-8">연락처 정보</h3>
             <ul className="list-inside list-disc">
@@ -35,7 +36,6 @@ export default async function StaffMemberPage({ params }: { params: { id: number
               </BulletRow>
             </ul>
           </article>
-          <HeaderAndList header="주요 업무" list={staff.tasks} />
         </div>
       </div>
     </PageLayout>
