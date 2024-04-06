408cbb117486e3cdda61d8abe7a30dc673033c8d
chansol <lchans0319@gmail.com>
2024-04-06 16:56:57 +0900

    fix: 새소식 캐러셀 모바일 버전 추가

---
+++ components/main/NewsCard.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { MainNews } from '@/types/main';
+
+import { formatMainNewsDateStr } from '@/utils/date';
+
+import { CARD_WIDTH_REM } from './constants';
+
+export default function NewsCard({ shadow, news }: { shadow: boolean; news: MainNews }) {
+  return (
+    <Link
+      href={`/community/news/${news.id}`}
+      style={{ width: `${CARD_WIDTH_REM}rem` }}
+      className={`flex h-[19rem] shrink-0 flex-col bg-neutral-50 ${
+        shadow ? 'shadow-[0_0_31.9px_0_rgba(0,0,0,0.07)]' : ''
+      }`}
+    >
+      <div className="relative h-[6.25rem] w-full">
+        <Image src={news.imageURL} fill alt="" className="object-cover" />
+      </div>
+
+      <div className="px-[0.87rem] pt-[0.88rem]">
+        <h3 className="line-clamp-2 text-[0.9375rem] font-semibold text-neutral-900">
+          {news.title}
+        </h3>
+        <time className="mt-3 block text-sm font-normal text-neutral-500">
+          {formatMainNewsDateStr(news.createdAt)}
+        </time>
+        <p className="mt-3 line-clamp-4 text-sm font-normal leading-[150%] text-neutral-500">
+          {news.description}
+        </p>
+      </div>
+    </Link>
+  );
+}
--- components/main/NewsSection.tsx
+++ components/main/NewsSection.tsx
@@ -6,25 +6,36 @@ import SmallRightArrow from '@/public/image/main/small_right_arrow.svg';
 
 import { MainNews } from '@/types/main';
 
+import useResponsive from '@/utils/hooks/useResponsive';
 import { getPath } from '@/utils/page';
 import { news } from '@/utils/segmentNode';
 
 import NewsCarousel from './NewsCarousel';
+import NewsCarouselMobile from './NewsCarouselMobile';
 
 export default function NewsSection({ mainNews }: { mainNews: MainNews[] }) {
-  return (
-    <div className="relative bg-neutral-100 sm:min-w-[1336px]">
-      <div className="absolute left-[3.81rem] top-[5rem] z-10 flex flex-col gap-2">
-        <h3 className="text-[1.75rem] font-medium text-neutral-950">새 소식</h3>
-        <Link
-          className="flex items-center gap-1 text-base font-normal text-[#E65615]"
-          href={getPath(news)}
-        >
-          더보기 <SmallRightArrow />
-        </Link>
-      </div>
+  const { isMobile } = useResponsive();
 
-      <NewsCarousel news={mainNews} />
+  return (
+    <div className="relative flex flex-col gap-[1.625rem] bg-neutral-100 pb-12 pl-5 pt-8 sm:block sm:min-w-[1336px] sm:p-0">
+      <Header />
+      {isMobile ? <NewsCarouselMobile news={mainNews} /> : <NewsCarousel news={mainNews} />}
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="flex flex-col gap-2 sm:absolute sm:left-[3.81rem] sm:top-[5rem] sm:z-10">
+      <h3 className="whitespace-nowrap text-[1.25rem] font-semibold text-neutral-950 sm:text-[1.75rem] sm:font-medium">
+        새 소식
+      </h3>
+      <Link
+        className="hidden items-center gap-1 text-base font-normal text-[#E65615] sm:flex"
+        href={getPath(news)}
+      >
+        더보기 <SmallRightArrow />
+      </Link>
     </div>
   );
 }
