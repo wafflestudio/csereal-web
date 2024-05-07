080e0a076572b0fdac4bef825c86aaa631f305ea
chansol <lchans0319@gmail.com>
2024-05-07 17:06:20 +0900

    feat: 장학제도 개별 페이지 메타데이터 추가

--- app/[locale]/academics/graduate/scholarship/[id]/page.tsx
+++ app/[locale]/academics/graduate/scholarship/[id]/page.tsx
@@ -1,9 +1,26 @@
+import { Metadata } from 'next';
+
 import { getScholarship } from '@/apis/academics';
 
 import ScholarshipDetail from '../../../helper/ScholarshipDetail';
 
 export default async function GraduateScholarshipPage({ params }: { params: { id: string } }) {
-  const scholarship = await getScholarship(parseInt(params.id));
+  const scholarship = await getData(parseInt(params.id));
 
   return <ScholarshipDetail scholarship={scholarship} />;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const scholarship = await getData(parseInt(params.id));
+
+  return {
+    title: `${scholarship.name}`,
+    description: '서울대학교 컴퓨터공학부 장학금 안내 페이지입니다.',
+  };
+}
+
+async function getData(id: number) {
+  const scholarship = await getScholarship(id);
+
+  return scholarship;
 }
