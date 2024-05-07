8a84ce10ef2acee2d5a4fbe9797fd8aeca68b2e0
chansol <lchans0319@gmail.com>
2024-05-07 18:07:08 +0900

    feat: 새소식 개별 페이지 메타데이터 추가

--- app/[locale]/community/news/[id]/page.tsx
+++ app/[locale]/community/news/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+import { getNewsDetail } from '@/apis/news';
 
 import PostFallback from '@/components/layout/fallback/PostFallback';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
@@ -7,17 +10,43 @@ import { PostSearchQueryParams } from '@/types/post';
 
 import NewsViewer from './NewsViewer';
 
+export async function generateMetadata({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: PostSearchQueryParams;
+}): Promise<Metadata> {
+  const news = await getData(parseInt(params.id), searchParams);
+
+  return {
+    title: `${news.title}`,
+    description: `서울대학교 컴퓨터공학부 새소식 페이지입니다.`,
+  };
+}
+
 interface NewsPostPageProps {
   params: { id: string };
   searchParams: PostSearchQueryParams;
 }
 
 export default async function NewsPostPage({ params, searchParams }: NewsPostPageProps) {
+  const id = parseInt(params.id);
+  if (Number.isNaN(id)) throw new Error('/news/[id]: id가 숫자가 아닙니다.');
+
+  const news = await getData(id, searchParams);
+
   return (
     <PageLayout titleType="big" bodyStyle={{ padding: 0 }}>
       <Suspense fallback={<PostFallback />}>
-        <NewsViewer id={parseInt(params.id)} searchParams={searchParams} />
+        <NewsViewer news={news} />
       </Suspense>
     </PageLayout>
   );
+}
+
+async function getData(id: number, searchParams: PostSearchQueryParams) {
+  const news = await getNewsDetail(id, searchParams);
+
+  return news;
 }
