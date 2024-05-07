57ed60ac96ce439518cee816a1adfb1f4c39fcf8
chansol <lchans0319@gmail.com>
2024-05-07 18:02:16 +0900

    feat: 세미나 개별 페이지 메타데이터 추가

--- app/[locale]/community/seminar/[id]/page.tsx
+++ app/[locale]/community/seminar/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+import { getSeminarPost } from '@/apis/seminar';
 
 import PostFallback from '@/components/layout/fallback/PostFallback';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
@@ -6,6 +9,21 @@ import PageLayout from '@/components/layout/pageLayout/PageLayout';
 import { PostSearchQueryParams } from '@/types/post';
 
 import SeminarViewer from './SeminarViewer';
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: PostSearchQueryParams;
+}): Promise<Metadata> {
+  const seminar = await getData(parseInt(params.id), searchParams);
+
+  return {
+    title: `${seminar.title}`,
+    description: `서울대학교 컴퓨터공학부 세미나 페이지입니다.`,
+  };
+}
 
 interface SeminarPostPageProps {
   params: { id: string };
@@ -16,11 +34,19 @@ export default async function SeminarPostPage({ params, searchParams }: SeminarP
   const id = parseInt(params.id);
   if (Number.isNaN(id)) throw new Error('/seminar/[id]: id가 숫자가 아닙니다.');
 
+  const seminar = await getData(id, searchParams);
+
   return (
     <PageLayout titleType="big" bodyStyle={{ padding: 0 }}>
       <Suspense fallback={<PostFallback />}>
-        <SeminarViewer id={id} searchParams={searchParams} />
+        <SeminarViewer seminar={seminar} />
       </Suspense>
     </PageLayout>
   );
+}
+
+async function getData(id: number, searchParams: PostSearchQueryParams) {
+  const seminar = await getSeminarPost(id, searchParams);
+
+  return seminar;
 }
