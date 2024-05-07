a89fbff3fd0c62106bf5295901c173e81c4aea7c
chansol <lchans0319@gmail.com>
2024-05-07 17:57:55 +0900

    feat: 공지사항 개별 페이지 메타데이터 추가

--- app/[locale]/community/notice/[id]/page.tsx
+++ app/[locale]/community/notice/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+import { getNoticePostDetail } from '@/apis/notice';
 
 import PostFallback from '@/components/layout/fallback/PostFallback';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
@@ -6,6 +9,21 @@ import PageLayout from '@/components/layout/pageLayout/PageLayout';
 import { PostSearchQueryParams } from '@/types/post';
 
 import NoticeViewer from './NoticeViewer';
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: PostSearchQueryParams;
+}): Promise<Metadata> {
+  const notice = await getData(parseInt(params.id), searchParams);
+
+  return {
+    title: `${notice.title}`,
+    description: `서울대학교 컴퓨터공학부 공지사항 페이지입니다.`,
+  };
+}
 
 interface NoticePostPageProps {
   params: { id: string };
@@ -21,10 +39,13 @@ export default async function NoticePostPage({
   // ID가 잘못된 경우 예외 처리
   if (Number.isNaN(id)) return <InvalidIDFallback rawID={rawID} />;
 
+  const notice = await getData(id, searchParams);
+
   return (
     <PageLayout titleType="big" bodyStyle={{ padding: 0 }}>
       <Suspense fallback={<PostFallback />}>
-        <NoticeViewer id={id} searchParams={searchParams} />
+        {/* <NoticeViewer id={id} searchParams={searchParams} /> */}
+        <NoticeViewer notice={notice} />
       </Suspense>
     </PageLayout>
   );
@@ -37,3 +58,9 @@ const InvalidIDFallback = ({ rawID }: { rawID: string }) => (
     </p>
   </PageLayout>
 );
+
+async function getData(id: number, searchParams: PostSearchQueryParams) {
+  const notice = await getNoticePostDetail(id, searchParams);
+
+  return notice;
+}
