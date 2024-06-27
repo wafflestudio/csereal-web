85f104edebeb813ff9587e92f8651535d2f4b568
chansol <lchans0319@gmail.com>
2024-06-27 17:24:29 +0900

    fix: 중요 안내 개수 제한 설명 삭제

--- app/[locale]/admin/helper/important/ImportantManagement.tsx
+++ app/[locale]/admin/helper/important/ImportantManagement.tsx
@@ -68,8 +68,9 @@ function ImportantDescription() {
     <p className="mb-10 bg-neutral-100 px-6 py-5 text-md leading-loose">
       메인페이지의 중요 안내에는 <strong>{`소식 > 공지사항, 새 소식, 세미나`}</strong> 중{' '}
       <strong>{`'중요 안내에 표시'`}</strong> 체크박스가 선택된 글들이 올라갑니다.
-      <br />이 목록에 2개 이상의 글이 포함되면 자동으로 최신글 2개만 표시되지만, 원활한 유지보수를
-      위하여 주기적인 관리가 필요합니다.
+      <br />
+      메인페이지에 보이는 중요 안내 개수 제한은 없지만, 원활한 유지보수를 위하여 주기적인 관리가
+      필요합니다.
     </p>
   );
 }
