f6d05ea3d7b7102e2a6a98657f298ce38b30e3e4
이성열 <yeolyi1310@gmail.com>
2024-06-28 18:23:18 +0900

    feat: sitemap에 소식탭 게시물들 추가 (#223)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -11,4 +11,19 @@ module.exports = {
       },
     ],
   },
+
+  additionalPaths: async () => {
+    const typeToPath = async (type) => {
+      const resp = await fetch(`https://cse-dev-waffle.bacchus.io/api/v1/${type}/ids`);
+      const idList = await resp.json();
+      return idList.flatMap((id) => [
+        { loc: `/community/${type}/${id}` },
+        { loc: `en/community/${type}/${id}` },
+      ]);
+    };
+
+    const promiseList = ['notice', 'news', 'seminar'].map(typeToPath);
+
+    return (await Promise.all(promiseList)).flatMap((x) => x);
+  },
 };
