2c80ad6a7f2dfa6def1554500e36ea0a5658cdce
chansol <lchans0319@gmail.com>
2024-07-08 10:45:43 +0900

    fix: 인터널 편집 주석 처리

--- app/.internal/InternalContent.tsx
+++ app/.internal/InternalContent.tsx
@@ -1,44 +1,46 @@
 'use client';
 
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
 
-import { putInternalAction } from '@/actions/internal';
+// import { putInternalAction } from '@/actions/internal';
 
-import LoginVisible from '@/components/common/LoginVisible';
-import BasicEditor, { BasicEditorContent } from '@/components/editor/BasicEditor';
+// import LoginVisible from '@/components/common/LoginVisible';
+// import BasicEditor, { BasicEditorContent } from '@/components/editor/BasicEditor';
 import HTMLViewer from '@/components/editor/HTMLViewer';
-import { GrayButton } from '@/components/editor/common/ActionButtons';
+// import { GrayButton } from '@/components/editor/common/ActionButtons';
 
-import { errorToast, successToast } from '@/utils/toast';
-import { handleServerAction } from '@/utils/serverActionError';
+// import { errorToast, successToast } from '@/utils/toast';
+// import { handleServerAction } from '@/utils/serverActionError';
 
 export default function InternalContent({ description }: { description: string }) {
-  const [isEditMode, toggleEditMode] = useReducer((x) => !x, false);
+  // const [isEditMode, toggleEditMode] = useReducer((x) => !x, false);
 
-  const handleComplete = async (content: BasicEditorContent) => {
-    try {
-      handleServerAction(await putInternalAction(content.description));
-      successToast('본문을 수정했습니다.');
-    } catch (e) {
-      errorToast('본문을 수정하지 못했습니다.');
-    }
-  };
+  // const handleComplete = async (content: BasicEditorContent) => {
+  //   try {
+  //     handleServerAction(await putInternalAction(content.description));
+  //     successToast('본문을 수정했습니다.');
+  //   } catch (e) {
+  //     errorToast('본문을 수정하지 못했습니다.');
+  //   }
+  // };
 
-  return isEditMode ? (
-    <BasicEditor
-      initialContent={{ description }}
-      actions={{
-        type: 'EDIT',
-        onComplete: handleComplete,
-        onCancel: toggleEditMode,
-      }}
-    />
-  ) : (
-    <>
-      <LoginVisible staff>
-        <GrayButton title="편집" onClick={toggleEditMode} disabled={false} />
-      </LoginVisible>
-      <HTMLViewer htmlContent={description} />
-    </>
-  );
+  return <HTMLViewer htmlContent={description} />;
+
+  // isEditMode ? (
+  //   <BasicEditor
+  //     initialContent={{ description }}
+  //     actions={{
+  //       type: 'EDIT',
+  //       onComplete: handleComplete,
+  //       onCancel: toggleEditMode,
+  //     }}
+  //   />
+  // ) : (
+  //   <>
+  //     <LoginVisible staff>
+  //       <GrayButton title="편집" onClick={toggleEditMode} disabled={false} />
+  //     </LoginVisible>
+  //     <HTMLViewer htmlContent={description} />
+  //   </>
+  // );
 }
