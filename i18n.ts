244e7df9d624c5fcd07971bbf41f81aa11ecb353
Yeolyi <yeolyi1310@gmail.com>
2024-07-12 20:15:48 +0900

    feat: getRequestConfig 예외처리

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,13 @@
+import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./messages/${locale}.json`)).default,
-}));
+import { locales } from './navigation';
+
+export default getRequestConfig(async ({ locale }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
+    messages: (await import(`./messages/${locale}.json`)).default,
+  };
+});
