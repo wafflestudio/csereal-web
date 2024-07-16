d9d0414ed9ad0566a6b67b5b832182b9fb82f520
chansol <lchans0319@gmail.com>
2024-07-16 18:04:41 +0900

    feat: Content-Security-Policy 헤더 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -135,6 +148,10 @@ const nextConfig = {
           {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
           },
         ],
       },
