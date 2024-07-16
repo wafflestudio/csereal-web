f553785182cf1699ef1d535a57bc4a100d4158b6
Yeolyi <yeolyi1310@gmail.com>
2024-07-16 23:15:44 +0900

    feat: 일부 페이지 force-dynamic

--- app/[locale]/about/history/page.tsx
+++ app/[locale]/about/history/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { Metadata } from 'next';
 
 import history_image from '@/public/image/about/history.png';
--- app/[locale]/about/student-clubs/page.tsx
+++ app/[locale]/about/student-clubs/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { Metadata } from 'next';
 import { ReactNode } from 'react';
 
