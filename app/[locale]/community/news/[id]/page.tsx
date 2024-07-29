d84d1d1f068c5d32624e2ec5b80a66f4d5303c3b
이성열 <yeolyi1310@gmail.com>
2024-07-30 00:15:58 +0900

    🐛 소식탭 게시물 상세에서 500이 뜨는 문제 해결 (#244)
    * feat: community 게시물 상세에서 500이 뜨는 문제
    
    * fix: common.ts 롤백

--- app/[locale]/community/news/[id]/page.tsx
+++ app/[locale]/community/news/[id]/page.tsx
@@ -4,6 +4,7 @@ import { getNewsDetail } from '@/apis/news';
 
 import PostFallback from '@/components/layout/fallback/PostFallback';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
+import InvalidIDFallback from '@/components/post/InvalidIDFallback';
 
 import { PostSearchQueryParams } from '@/types/post';
 
@@ -16,15 +17,19 @@ export async function generateMetadata({
   params: { locale, id },
   searchParams,
 }: NewsPostPageProps) {
-  const newsPost = await getNewsDetail(parseInt(id), searchParams);
+  try {
+    const newsPost = await getNewsDetail(parseInt(id), searchParams);
 
-  return await getMetadata({
-    locale,
-    node: news,
-    metadata: {
-      title: `${newsPost.title}`,
-    },
-  });
+    return await getMetadata({
+      locale,
+      node: news,
+      metadata: {
+        title: `${newsPost.title}`,
+      },
+    });
+  } catch {
+    return {};
+  }
 }
 
 interface NewsPostPageProps {
@@ -36,13 +41,17 @@ export default async function NewsPostPage({ params, searchParams }: NewsPostPag
   const id = parseInt(params.id);
   if (Number.isNaN(id)) throw new Error('/news/[id]: id가 숫자가 아닙니다.');
 
-  const news = await getNewsDetail(id, searchParams);
+  try {
+    const news = await getNewsDetail(id, searchParams);
 
-  return (
-    <PageLayout titleType="big" bodyStyle={{ padding: 0 }}>
-      <Suspense fallback={<PostFallback />}>
-        <NewsViewer news={news} />
-      </Suspense>
-    </PageLayout>
-  );
+    return (
+      <PageLayout titleType="big" bodyStyle={{ padding: 0 }}>
+        <Suspense fallback={<PostFallback />}>
+          <NewsViewer news={news} />
+        </Suspense>
+      </PageLayout>
+    );
+  } catch {
+    return <InvalidIDFallback rawID={params.id} />;
+  }
 }
--- app/[locale]/community/seminar/[id]/page.tsx
+++ app/[locale]/community/seminar/[id]/page.tsx
@@ -4,6 +4,7 @@ import { getSeminarPost } from '@/apis/seminar';
 
 import PostFallback from '@/components/layout/fallback/PostFallback';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
+import InvalidIDFallback from '@/components/post/InvalidIDFallback';
 
 import { PostSearchQueryParams } from '@/types/post';
 
@@ -16,15 +17,19 @@ export async function generateMetadata({
   params: { locale, id },
   searchParams,
 }: SeminarPostPageProps) {
-  const seminarPost = await getSeminarPost(parseInt(id), searchParams);
+  try {
+    const seminarPost = await getSeminarPost(parseInt(id), searchParams);
 
-  return await getMetadata({
-    locale,
-    node: seminar,
-    metadata: {
-      title: `${seminarPost.title}`,
-    },
-  });
+    return await getMetadata({
+      locale,
+      node: seminar,
+      metadata: {
+        title: `${seminarPost.title}`,
+      },
+    });
+  } catch {
+    return {};
+  }
 }
 
 interface SeminarPostPageProps {
@@ -36,13 +41,17 @@ export default async function SeminarPostPage({ params, searchParams }: SeminarP
   const id = parseInt(params.id);
   if (Number.isNaN(id)) throw new Error('/seminar/[id]: id가 숫자가 아닙니다.');
 
-  const seminar = await getSeminarPost(id, searchParams);
+  try {
+    const seminar = await getSeminarPost(id, searchParams);
 
-  return (
-    <PageLayout titleType="big" bodyStyle={{ padding: 0 }}>
-      <Suspense fallback={<PostFallback />}>
-        <SeminarViewer seminar={seminar} />
-      </Suspense>
-    </PageLayout>
-  );
+    return (
+      <PageLayout titleType="big" bodyStyle={{ padding: 0 }}>
+        <Suspense fallback={<PostFallback />}>
+          <SeminarViewer seminar={seminar} />
+        </Suspense>
+      </PageLayout>
+    );
+  } catch {
+    return <InvalidIDFallback rawID={params.id} />;
+  }
 }
