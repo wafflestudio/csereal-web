26d8c2016626d8b9ddae4dd230f656830fb23ba2
chansol <lchans0319@gmail.com>
2024-07-29 14:22:38 +0900

    refactor: ContentViewer 리팩토링

--- app/[locale]/academics/helper/CourseChanges.tsx
+++ app/[locale]/academics/helper/CourseChanges.tsx
@@ -9,7 +9,7 @@ import PageLayout from '@/components/layout/pageLayout/PageLayout';
 
 import { CourseChange } from '@/types/academics';
 
-const YEAR_LIMIT_CNT = 10;
+const YEAR_LIMIT_CNT = 5; // 양수
 
 export default function CourseChanges({ changes }: { changes: CourseChange[] }) {
   const [selectedYear, setSelectedYear] = useState(changes[0].year);
@@ -25,47 +25,58 @@ export default function CourseChanges({ changes }: { changes: CourseChange[] }) 
         setSelectedTime={setSelectedYear}
       />
       <div className="mt-7">
-        {selectedChanges.map((change, i) => (
+        {selectedChanges.length === 1 ? (
           <ContentViewer
-            description={change.description}
-            year={change.year}
-            expandDefault={i === 0}
-            alwaysExpanded={selectedChanges.length === 1}
-            isLast={i !== 0 && i === selectedChanges.length - 1}
-            key={change.year}
+            description={selectedChanges[0].description}
+            year={selectedChanges[0].year}
           />
-        ))}
+        ) : (
+          selectedChanges.map((change, i) => (
+            <TogglableContentViewer
+              description={change.description}
+              year={change.year}
+              expandDefault={i === 0}
+              isLast={i !== 0 && i === selectedChanges.length - 1}
+              key={change.year}
+            />
+          ))
+        )}
       </div>
     </PageLayout>
   );
 }
 
-function ContentViewer({
+function ContentViewer({ description, year }: { description: string; year: number }) {
+  return (
+    <div className="mb-5">
+      <span className="font-semibold">{year}학년도 교과과정 변경 내역</span>
+      <HTMLViewer htmlContent={description} />
+    </div>
+  );
+}
+
+function TogglableContentViewer({
   description,
   year,
   expandDefault = false,
-  alwaysExpanded = false,
   isLast = false,
 }: {
   description: string;
   year: number;
   expandDefault?: boolean;
-  alwaysExpanded?: boolean;
   isLast?: boolean;
 }) {
-  const [isExpanded, toggleContent] = useReducer((x) => !x, expandDefault || alwaysExpanded);
+  const [isExpanded, toggleContent] = useReducer((x) => !x, expandDefault);
 
   return (
     <div className="mb-5">
-      <button onClick={alwaysExpanded ? undefined : toggleContent} className="flex items-center">
+      <button onClick={toggleContent} className="flex items-center">
         <span className="font-semibold">
           {year}학년도{isLast && ' 이하'} 교과과정 변경 내역
         </span>
-        {!alwaysExpanded && (
-          <span className="material-symbols-outlined text-3xl font-light">
-            {isExpanded ? 'expand_less' : 'expand_more'}
-          </span>
-        )}
+        <span className="material-symbols-outlined text-3xl font-light">
+          {isExpanded ? 'expand_less' : 'expand_more'}
+        </span>
       </button>
       {isExpanded && <HTMLViewer htmlContent={description} />}
     </div>
