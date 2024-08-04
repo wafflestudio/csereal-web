7f27d473c4174e1c01137abb73d06c154e2c8863
Limchansol <lchans0319@gmail.com>
2024-08-04 14:42:52 +0900

    ♻️ 타임라인과 교과과정 리뷰 반영 (#243)

--- app/[locale]/academics/helper/TimeLine.tsx
+++ app/[locale]/academics/helper/TimeLine.tsx
@@ -30,11 +30,11 @@ interface TimeSpotProps {
 function TimeSpot({ time, isSelected, onChange, isLast }: TimeSpotProps) {
   return (
     <label
-      className={`group relative mb-7 mr-7 flex h-[38px] items-center ${
+      className={`group relative mb-7 mr-11 flex h-[38px] items-center ${
         isSelected ? 'cursor-default' : 'cursor-pointer'
       }`}
     >
-      <div className="flex h-full w-[1.875rem] flex-col items-center justify-between">
+      <div className="flex h-full w-9 flex-col items-center justify-between">
         <Circle highlight={isSelected} />
         <span className="flex items-center text-sm tracking-[0.02em] text-main-orange">
           {time}
@@ -43,7 +43,7 @@ function TimeSpot({ time, isSelected, onChange, isLast }: TimeSpotProps) {
           )}
         </span>
       </div>
-      <div className="absolute left-5 top-1.5 h-px w-12 bg-main-orange" />
+      <div className="absolute left-5 top-1.5 h-px w-[72px] bg-main-orange" />
       <input
         type="radio"
         name="time"
@@ -57,9 +57,9 @@ function TimeSpot({ time, isSelected, onChange, isLast }: TimeSpotProps) {
 }
 
 function Circle({ highlight }: { highlight: boolean }) {
-  const highlightStyle = 'bg-main-orange w-3.5 h-3.5 mt-0';
+  const highlightStyle = 'bg-main-orange w-4 h-4 mt-0';
   const defaultStyle =
-    'bg-white w-2.5 h-2.5 mt-[2px] group-hover:w-3.5 group-hover:h-3.5 group-hover:mt-0 z-10';
+    'bg-white w-3 h-3 mt-[2px] group-hover:w-3.5 group-hover:h-3.5 group-hover:mt-0 z-10';
 
   return (
     <span
