e3f8f6ec40ce1700f0d63bf8909d2e7eee3f2f16
chansol <lchans0319@gmail.com>
2024-08-05 15:02:34 +0900

    fix: 편집 후 revalidate

--- apis/people.ts
+++ apis/people.ts
@@ -1,4 +1,4 @@
-import { notFound } from 'next/navigation';
+import { FETCH_TAG_STAFF } from '@/constants/network';
 
 import { Language, WithLanguage } from '@/types/language';
 import {
@@ -25,6 +25,8 @@ import {
 const facultyPath = '/professor';
 const staffPath = '/staff';
 
+/* 교수진 */
+
 export const getActiveFacultyList = (language: Language) =>
   getRequest<FacultyList>('/professor/active', { language });
 
@@ -34,18 +36,6 @@ export const getEmeritusFacultyList = (language: Language) =>
   getRequest<SimpleEmiritusFaculty[]>('/professor/inactive', { language });
 
 export const getEmeritusFaculty = (id: number) => getRequest<EmiritusFaculty>(`/professor/${id}`);
-
-export const getStaffList = (language: Language) =>
-  getRequest<SimpleStaff[]>('/staff', { language });
-
-export const getStaff = (id: number) => {
-  try {
-    return getRequest2<WithLanguage<Staff>>(`/staff/${id}`);
-  } catch (e) {
-    console.log('eeee');
-    notFound();
-  }
-};
 
 export const postFaculty = async (formData: FormData) => {
   return postRequest(facultyPath, { body: formData, jsessionID: true }) as Promise<{
@@ -60,6 +50,17 @@ export const putFaculty = async (id: number, formData: FormData) => {
 
 export const deleteFaculty = async (id: number) =>
   deleteRequest(`${facultyPath}/${id}`, { jsessionID: true });
+
+/* 행정직원 */
+// TODO: /api/v1 v2 통합되면 request 메소드 원래대로 교체
+
+export const getStaffList = (language: Language) =>
+  getRequest2<SimpleStaff[]>('/staff', { language }, { next: { tags: [FETCH_TAG_STAFF] } });
+
+export const getStaff = (id: number) =>
+  getRequest2<WithLanguage<Staff>>(`/staff/${id}`, undefined, {
+    next: { tags: [FETCH_TAG_STAFF] },
+  });
 
 export const postStaff = async (formData: FormData) => {
   return postRequest2(staffPath, { body: formData, jsessionID: true }) as Promise<{
