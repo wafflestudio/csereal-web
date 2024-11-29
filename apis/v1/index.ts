cd136d23b996cfa0d9cfd70d28c84a1f37993db6
leo.yi <leo.yi@kakaocorp.com>
2024-11-29 16:10:37 +0900

    refactor: getMain 폴더구조 수정

--- apis/v1/index.ts
+++ apis/v1/index.ts
@@ -1,9 +1,9 @@
 import { FETCH_TAG_NEWS, FETCH_TAG_NOTICE } from '@/constants/network';
 import { MainResponse } from '@/types/main';
 
-import { getRequest } from '.';
+import { getRequest } from '..';
 
 export const getMain = () =>
-  getRequest<MainResponse>('', undefined, {
+  getRequest<MainResponse>('/v1', undefined, {
     next: { tags: [FETCH_TAG_NEWS, FETCH_TAG_NOTICE] },
   });
