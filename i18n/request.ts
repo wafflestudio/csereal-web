5cfb092558dabb7a8278d8277408edee65241c1d
leo.yi <leo.yi@kakaocorp.com>
2024-11-29 19:21:15 +0900

    refactor: next-intl 설정 수정

--- i18n/request.ts
+++ i18n/request.ts
@@ -3,10 +3,8 @@ import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
-  // Ensure that a valid locale is used
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale;
--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,13 +2,10 @@ import { createNavigation } from 'next-intl/navigation';
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ['en', 'ko'],
-
-  // Used when no locale matches
   defaultLocale: 'ko',
+  localePrefix: 'as-needed',
+  localeDetection: false,
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
