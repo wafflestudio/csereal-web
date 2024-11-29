9773590c4fcf4e82017ab7fd4053ec79a6916084
leo.yi <leo.yi@kakaocorp.com>
2024-11-29 23:19:45 +0900

    chore: react 관련 eslint plugin 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,45 @@
-import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { includeIgnoreFile } from '@eslint/compat';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  includeIgnoreFile(gitignorePath),
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.serviceworker,
+      },
+    },
+    settings: { react: { version: 'detect' } },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  ...pluginReactHooks.configs.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
   {
-    plugins: { 'simple-import-sort': simpleImportSort },
-    rules: { 'simple-import-sort/imports': 'error', 'simple-import-sort/exports': 'error' },
+    plugins: { 'react-hooks': pluginReactHooks, 'simple-import-sort': simpleImportSort },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
   },
   eslintConfigPrettier,
 ];
