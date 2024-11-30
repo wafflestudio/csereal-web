a2d1f33c3498630ba44ce2a8a8dc2cbcd2998082
leo.yi <leo.yi@kakaocorp.com>
2024-11-30 19:35:51 +0900

    feat: 공식 홈페이지 새 창에서 열기

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -98,7 +98,12 @@ function BuildVersion() {
   return (
     <div className="fixed right-0 top-0 z-50 bg-[royalblue] p-2 text-sm font-semibold text-white">
       <p>Beta Version: {buildVersion}</p>
-      <Link className="text-white underline" href="https://cse.snu.ac.kr">
+      <Link
+        className="text-white underline"
+        href="https://cse.snu.ac.kr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         👉 공식 홈페이지 가기
       </Link>
     </div>
