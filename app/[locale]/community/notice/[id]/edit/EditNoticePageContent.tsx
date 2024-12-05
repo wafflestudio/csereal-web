3f28aa2e27dca29aa7a61f459522072862336b0e
leo.yi <leo.yi@kakaocorp.com>
2024-12-05 19:20:23 +0900

    refactor: 공지사항에서 rhf 사용

--- app/[locale]/community/notice/[id]/edit/EditNoticePageContent.tsx
+++ app/[locale]/community/notice/[id]/edit/EditNoticePageContent.tsx
@@ -3,17 +3,13 @@
 import { useTransition } from 'react';
 
 import { deleteNoticeAction, patchNoticeAction } from '@/actions/notice';
-import PostEditor from '@/components/editor/PostEditor';
-import {
-  isLocalFile,
-  isUploadedFile,
-  PostEditorContent,
-} from '@/components/editor/PostEditorTypes';
+import NoticeEditor, {
+  NoticeFormData,
+} from '@/app/[locale]/community/notice/components/NoticeEditor';
+import { isLocalFile, isUploadedFile } from '@/components/editor/PostEditorTypes';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
-import { NOTICE_TAGS } from '@/constants/tag';
 import { useRouter } from '@/i18n/routing';
 import { Notice } from '@/types/notice';
-import { validateNoticeForm } from '@/utils/formValidation';
 import { getPath } from '@/utils/page';
 import { notice } from '@/utils/segmentNode';
 import { encodeFormDataFileName } from '@/utils/string';
@@ -25,32 +21,56 @@ export default function EditNoticePageContent({ id, data }: { id: number; data: 
   const router = useRouter();
   const [, startTransition] = useTransition();
 
-  const initialContent: PostEditorContent = {
+  const defaultValues: NoticeFormData = {
     title: data.title,
     titleForMain: data.titleForMain ?? '',
     description: data.description,
     attachments: data.attachments.map((file) => ({ type: 'UPLOADED_FILE', file })),
-
     tags: data.tags,
     isPrivate: data.isPrivate,
     isImportant: data.isImportant,
     isPinned: data.isPinned,
-
-    mainImage: null,
-    isSlide: false,
-
-    date: new Date().toISOString(),
   };
 
-  const handleCancel = () => router.push(`${noticePath}/${id}`);
+  const onCancel = () => router.push(`${noticePath}/${id}`);
 
-  const handleComplete = async (content: PostEditorContent) => {
-    validateNoticeForm(content);
-    const formData = contentToFormData(data, content);
+  const onSubmit = async (content: NoticeFormData) => {
+    const uploadedAttachments = content.attachments.filter(isUploadedFile).map((x) => x.file);
+    const localAttachments = content.attachments.filter(isLocalFile).map((x) => x.file);
+
+    const deleteIds = data.attachments
+      .map((x) => x.id)
+      .filter((id1) => uploadedAttachments.find((x) => x.id === id1) === undefined);
+
+    const formData = new FormData();
+
+    formData.append(
+      'request',
+      new Blob(
+        [
+          JSON.stringify({
+            title: content.title,
+            titleForMain: content.titleForMain ? content.titleForMain : null,
+            description: content.description,
+            isPrivate: content.isPrivate,
+            isPinned: content.isPinned,
+            isImportant: content.isImportant,
+            tags: content.tags,
+            deleteIds,
+          }),
+        ],
+        {
+          type: 'application/json',
+        },
+      ),
+    );
+
+    encodeFormDataFileName(formData, 'newAttachments', localAttachments);
+
     await patchNoticeAction(id, formData);
   };
 
-  const handleDelete = async () => {
+  const onDelete = async () => {
     startTransition(async () => {
       const result = await deleteNoticeAction(id);
       if (result) {
@@ -63,54 +83,12 @@ export default function EditNoticePageContent({ id, data }: { id: number; data: 
 
   return (
     <PageLayout title="공지사항 편집" titleType="big" titleMargin="mb-[2.25rem]" hideNavbar>
-      <PostEditor
-        tags={NOTICE_TAGS}
-        showIsPinned
-        showIsImportant
-        actions={{
-          type: 'EDIT',
-          onCancel: handleCancel,
-          onSubmit: handleComplete,
-          onDelete: handleDelete,
-        }}
-        initialContent={initialContent}
+      <NoticeEditor
+        onCancel={onCancel}
+        onSubmit={onSubmit}
+        onDelete={onDelete}
+        defaultValues={defaultValues}
       />
     </PageLayout>
   );
 }
-
-const contentToFormData = (prevNotice: Notice, content: PostEditorContent) => {
-  const uploadedAttachments = content.attachments.filter(isUploadedFile).map((x) => x.file);
-  const localAttachments = content.attachments.filter(isLocalFile).map((x) => x.file);
-
-  const deleteIds = prevNotice.attachments
-    .map((x) => x.id)
-    .filter((id1) => uploadedAttachments.find((x) => x.id === id1) === undefined);
-
-  const formData = new FormData();
-
-  formData.append(
-    'request',
-    new Blob(
-      [
-        JSON.stringify({
-          title: content.title,
-          titleForMain: content.titleForMain ? content.titleForMain : null,
-          description: content.description,
-          isPrivate: content.isPrivate,
-          isPinned: content.isPinned,
-          isImportant: content.isImportant,
-          tags: content.tags,
-          deleteIds,
-        }),
-      ],
-      {
-        type: 'application/json',
-      },
-    ),
-  );
-
-  encodeFormDataFileName(formData, 'newAttachments', localAttachments);
-
-  return formData;
-};
