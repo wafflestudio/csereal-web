440b514d026f2accb06179321cd80b7a88b9580d
이성열 <yeolyi1310@gmail.com>
2025-01-16 12:02:07 +0900

    🐛 메인 검수 (#294)

--- components/common/Image.tsx
+++ components/common/Image.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from 'clsx';
 // eslint-disable-next-line no-restricted-imports
 import NextImage, { getImageProps } from 'next/image';
 import { ComponentProps } from 'react';
@@ -14,6 +15,13 @@ export default function Image(props: ComponentProps<typeof NextImage>) {
   return (
     <img
       {...delegated}
+      className={clsx(
+        {
+          // 메인화면 background.png에 CLS가 발생해 수동으로 처리
+          absolute: props.fill,
+        },
+        props.className,
+      )}
       alt={alt}
       {...useStyle((_style) => {
         Object.assign(_style, style);
