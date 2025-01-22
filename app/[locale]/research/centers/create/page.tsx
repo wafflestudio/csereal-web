0aacd4ff2dc1af85f9a83e00bf86b4fee855d208
chansol <lchans0319@gmail.com>
2025-01-22 17:50:01 +0900

    fix: handleServerAction을 handleServerResponse로 대체

--- app/[locale]/research/centers/create/page.tsx
+++ app/[locale]/research/centers/create/page.tsx
@@ -2,22 +2,16 @@
 
 import { postResearchCenterAction } from '@/actions/research';
 import PageLayout from '@/components/layout/pageLayout/PageLayout';
-import { errorToStr } from '@/utils/error';
 import { contentToFormData } from '@/utils/formData';
-import { handleServerAction } from '@/utils/serverActionError';
-import { errorToast, successToast } from '@/utils/toast';
+import { handleServerResponse } from '@/utils/serverActionError';
 
 import ResearchCenterEditor, { ResearchCenterFormData } from '../components/ResearchCenterEditor';
 
 export default function ResearchCenterCreatePage() {
   const onSubmit = async ({ image, ...requestObject }: ResearchCenterFormData) => {
     const formData = contentToFormData('CREATE', { requestObject, image });
-    try {
-      handleServerAction(await postResearchCenterAction(formData));
-      successToast('연구 센터를 추가했습니다.');
-    } catch (e) {
-      errorToast(errorToStr(e));
-    }
+    const resp = await postResearchCenterAction(formData);
+    handleServerResponse(resp, { successMessage: '연구 센터를 추가했습니다.' });
   };
 
   return (
