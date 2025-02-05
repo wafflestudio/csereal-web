de7d97c830e2e2260709016b8f729d8af816d827
leo.yi <leo.yi@kakaocorp.com>
2025-02-05 17:46:33 +0900

    fix: 검색 v2 반영

--- apis/v2/academics/search/top.ts
+++ apis/v2/academics/search/top.ts
@@ -2,4 +2,4 @@ import { getRequest } from '@/apis';
 import { AcademicsSearchResult, SearchParam } from '@/apis/types/search';
 
 export const searchAcademics = (params: SearchParam) =>
-  getRequest('/v1/academics/search/top', params) as Promise<AcademicsSearchResult>;
+  getRequest('/v2/academics/search/top', params) as Promise<AcademicsSearchResult>;
