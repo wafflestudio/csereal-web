c995590d76af83ad637af5d674060674a8f8b9e8
chansol <lchans0319@gmail.com>
2025-02-07 17:00:42 +0900

    remove: 사용하지 않는 입학 GET api들 제거

--- apis/v2/admissions/graduate/regular-admission.ts
+++
@@ -1,7 +0,0 @@
-import { getRequest } from '@/apis';
-import { WithLanguage } from '@/types/language';
-
-export const getGraduateRegularAdmission = () =>
-  getRequest('/v2/admissions/graduate/regular-admission') as Promise<
-    WithLanguage<{ description: string }>
-  >;
--- apis/v2/admissions/international/exchange-visiting.ts
+++
@@ -1,7 +0,0 @@
-import { getRequest } from '@/apis';
-import { WithLanguage } from '@/types/language';
-
-export const getInternationalExchangeVisiting = () =>
-  getRequest('/v2/admissions/international/exchange-visiting') as Promise<
-    WithLanguage<{ description: string }>
-  >;
