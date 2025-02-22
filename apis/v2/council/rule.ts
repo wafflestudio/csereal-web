11c547518cae67023cc93010cb0a60cf404c1b0d
leo.yi <leo.yi@kakaocorp.com>
2025-02-22 17:54:53 +0900

    fix: 불필요한 tag 제거

--- apis/v2/council/rule.ts
+++ apis/v2/council/rule.ts
@@ -1,13 +1,9 @@
 import { getRequest } from '@/apis';
 import { Attachment } from '@/apis/types/attachment';
-import { FETCH_TAG_INTRO } from '@/constants/network';
 
 type Response = {
   constitution: { type: string; attachments: Attachment[] };
   bylaw: { type: string; attachments: Attachment[] };
 };
 
-export const getCouncilRule = () =>
-  getRequest<Response>('/v2/council/rule', undefined, {
-    next: { tags: [FETCH_TAG_INTRO] },
-  });
+export const getCouncilRule = () => getRequest<Response>('/v2/council/rule', undefined);
