113ce87cf164d56471aeef1547c0baa027a048c3
leo.yi <leo.yi@kakaocorp.com>
2025-02-22 17:52:16 +0900

    feat: 활동 보고 페이지 추가

---
+++ app/[locale]/community/council/report/page.tsx
@@ -0,0 +1,7 @@
+import PageLayout from '@/components/layout/pageLayout/PageLayout';
+
+export const dynamic = 'force-dynamic';
+
+export default async function CouncilIntroPage() {
+  return <PageLayout titleType="big">TODO</PageLayout>;
+}
