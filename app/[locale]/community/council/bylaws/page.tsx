32fd926ec2ae4e2437f6f76bab5c64fae2ead1b9
leo.yi <leo.yi@kakaocorp.com>
2025-02-22 16:40:01 +0900

    feat: 학칙 페이지를 세그먼트 노드와 번역에 추가

---
+++ app/[locale]/community/council/bylaws/page.tsx
@@ -0,0 +1,11 @@
+export const dynamic = 'force-dynamic';
+
+import PageLayout from '@/components/layout/pageLayout/PageLayout';
+
+export default async function CouncilIntroPage() {
+  return (
+    <PageLayout titleType="big" removePadding>
+      TODO
+    </PageLayout>
+  );
+}
