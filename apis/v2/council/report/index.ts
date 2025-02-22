03edde6747953db58c850fd8d8314076aaa0e327
leo.yi <leo.yi@kakaocorp.com>
2025-02-22 18:04:20 +0900

    feat: report 관련 api 추가

---
+++ apis/v2/council/report/index.ts
@@ -0,0 +1,22 @@
+import { getRequest, postRequest } from '@/apis';
+import { FETCH_TAG_COUNCIL_REPORT } from '@/constants/network';
+
+interface GETReportResponse {
+  total: number;
+  reports: {
+    id: number;
+    title: string;
+    sequence: number;
+    name: string;
+    createdAt: string;
+    imageURL: string;
+  }[];
+}
+
+export const getCouncilReportList = () =>
+  getRequest<GETReportResponse>('/v2/council/report', undefined, {
+    next: { tags: [FETCH_TAG_COUNCIL_REPORT] },
+  });
+
+export const postCouncilReport = (body: FormData) =>
+  postRequest('/v2/council/report', { body, jsessionID: true });
