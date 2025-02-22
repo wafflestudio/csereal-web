06e5e659ceb3fa8140d68469e769b40cae81ecca
leo.yi <leo.yi@kakaocorp.com>
2025-02-22 21:21:12 +0900

    실패하는 테스트 추가

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,3 +4,8 @@ test('has title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/서울대학교 컴퓨터공학부/);
 });
+
+test('실패하는 테스트', async ({ page }) => {
+  await page.goto('/');
+  expect(1).toBe(2);
+});
