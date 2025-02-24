8b809c93bfe4dec5b76f04a2c9eef2f77cdaf7fc
leo.yi <leo.yi@kakaocorp.com>
2025-02-24 19:23:41 +0900

    feat: LoginVisible 컴포넌트 기능 확장

--- components/common/LoginVisible.tsx
+++ components/common/LoginVisible.tsx
@@ -2,18 +2,28 @@
 
 import { ReactNode } from 'react';
 
+import { Role } from '@/apis/types/role';
 import { useSessionContext } from '@/contexts/SessionContext';
 
 type Props = {
-  staff?: boolean;
   fallback?: ReactNode;
   children: ReactNode;
+  role?: Role[] | Role;
+
+  // role='ROLE_STAFF'일때와 동일
+  staff?: boolean;
 };
 
-export default function LoginVisible({ staff, children, fallback }: Props) {
+export default function LoginVisible({ staff, children, fallback, role }: Props) {
   const { state } = useSessionContext();
+
   if (state === 'logout') return fallback;
   if (staff && state !== 'ROLE_STAFF') return fallback;
+
+  if (role) {
+    const roleArr = Array.isArray(role) ? role : [role];
+    if (!roleArr.includes(state)) return fallback;
+  }
 
   return children;
 }
