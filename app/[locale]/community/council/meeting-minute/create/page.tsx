6efdf98355fd743de1fea729e42d6a0dd6209d24
chansol <lchans0319@gmail.com>
2025-02-25 16:55:23 +0900

    fix: year index NaN 에러 처리

--- app/[locale]/community/council/meeting-minute/create/page.tsx
+++ app/[locale]/community/council/meeting-minute/create/page.tsx
@@ -10,14 +10,17 @@ interface MinuteCreatePageProps {
 
 const minutePath = getPath(councilMinute);
 
-export default async function CouncilMinuteCreatePage({ searchParams }: MinuteCreatePageProps) {
-  const { year } = await searchParams;
+export default async function CouncilMinuteCreatePage(props: MinuteCreatePageProps) {
+  const searchParams = await props.searchParams;
+
+  const year = Number(searchParams.year);
+  if (Number.isNaN(year)) throw new Error('/meeting-minute?year=[year]: year가 숫자가 아닙니다.');
 
   return (
     // TODO: 영문 번역
     <PageLayout title={`${year ? `${year}년 ` : ''}학생회 회의록 추가`} titleType="big">
       <CouncilMeetingMinuteEditor
-        option={{ type: 'CREATE', defaultValues: year ? { year: Number(year) } : undefined }}
+        option={{ type: 'CREATE', defaultValues: { year } }}
         cancelPath={minutePath}
       />
     </PageLayout>
--- app/[locale]/community/council/meeting-minute/edit/page.tsx
+++ app/[locale]/community/council/meeting-minute/edit/page.tsx
@@ -12,13 +12,18 @@ interface MinuteEditPageProps {
 
 const minutePath = getPath(councilMinute);
 
-export default async function CouncilMinuteEditPage({ searchParams }: MinuteEditPageProps) {
-  const year = Number((await searchParams).year);
-  const index = Number((await searchParams).index);
+export default async function CouncilMinuteEditPage(props: MinuteEditPageProps) {
+  const searchParams = await props.searchParams;
+  const year = Number(searchParams.year);
+  const index = Number(searchParams.index);
+
+  if (Number.isNaN(year) || Number.isNaN(index))
+    throw new Error('/meeting-minute?year=[year]&index=[index]: year나 index가 숫자가 아닙니다.');
 
   const data = await getCouncilMinute(year, index);
 
   return (
+    // TODO: 영문 번역
     <PageLayout title={`${year}년 학생회 ${index}차 회의록 편집`} titleType="big">
       <CouncilMeetingMinuteEditor
         option={{
