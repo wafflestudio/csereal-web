dbdb454e75f7ebd5d0f34a05c3e0b0a7d9beed0e
goranikin <goranikin@snu.ac.kr>
2025-03-26 08:31:34 +0900

    🐛 Fix: 취소 모달창 포커스 수정

--- components/common/Buttons.tsx
+++ components/common/Buttons.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { MouseEventHandler } from 'react';
+import { useFormStatus } from 'react-dom';
 
 import { Link } from '@/i18n/routing';
 import useModal from '@/utils/hooks/useModal';
@@ -104,3 +107,29 @@ export function EditButton({ href }: { href: string }) {
     </Link>
   );
 }
+
+export const ConfirmButton = ({
+  title,
+  buttonRef,
+  onClick,
+}: {
+  title: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  buttonRef?: React.RefObject<HTMLButtonElement | null>;
+}) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className={`ml-2.5 h-[2.1875rem] rounded-[.0625rem] bg-neutral-700 px-[.875rem] py-[.3125rem] text-md font-medium leading-[1.5rem] text-white hover:bg-neutral-500 ${
+        pending && 'opacity-30'
+      }`}
+      disabled={pending}
+      type="submit"
+      onClick={onClick}
+      ref={buttonRef}
+    >
+      {title}
+    </button>
+  );
+};
