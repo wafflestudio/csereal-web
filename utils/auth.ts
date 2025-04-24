d9676f6f4882cd7d75563cc04d1cb8eedbacd44e
goranikin <goranikin@snu.ac.kr>
2025-04-24 21:16:17 +0900

    🔀 Merge

---
+++ utils/auth.ts
@@ -0,0 +1,27 @@
+import { UserState } from '@/contexts/SessionContext';
+
+/**
+ * 페이지별 권한 검사
+ * 권한간의 계층관계가 명확하지 않으므로(요구사항이 바뀔 수도 있으므로) 배열로 반환
+ */
+export const getRequiredAuth = (pathname: string): UserState[] => {
+  if (pathname.startsWith('/en')) pathname = pathname.slice(3);
+
+  const segments = pathname.split('/').filter(Boolean);
+  const firstSegment = segments[0];
+  const lastSegment = segments.at(-1);
+
+  const isCouncilPage = segments.includes('council');
+  const isCreateOrEditPage = lastSegment === 'create' || lastSegment === 'edit';
+  const isAdminPage = firstSegment === 'admin';
+
+  if (isCouncilPage && isCreateOrEditPage) {
+    return ['ROLE_COUNCIL', 'ROLE_STAFF'];
+  }
+
+  if (isAdminPage || isCreateOrEditPage) {
+    return ['ROLE_STAFF'];
+  }
+
+  return ['ROLE_STAFF', 'ROLE_RESERVATION', 'ROLE_COUNCIL', 'logout'];
+};
