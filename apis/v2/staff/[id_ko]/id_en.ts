aae274e9cdb1a4dbc1de106da4564b55f76e5bf1
Limchansol <lchans0319@gmail.com>
2025-04-28 16:48:42 +0900

    Revert "🐛 staff PUT api 주소 오타 수정 (#392)"
    This reverts commit 0ae74f58aab8d0cfd599ae8b83af84fc6ac9258d.

--- apis/v2/staff/[id_ko]/id_en.ts
+++ apis/v2/staff/[id_ko]/id_en.ts
@@ -2,7 +2,7 @@ import { deleteRequest, putRequest } from '@/apis';
 import { WithLanguage } from '@/types/language';
 
 export const putStaff = (ids: WithLanguage<number>, formData: FormData) =>
-  putRequest(`/v2/staff/${ids.ko}/${ids.en}`, { body: formData, jsessionID: true });
+  putRequest(`'/v2/staff/${ids.ko}/${ids.en}`, { body: formData, jsessionID: true });
 
 export const deleteStaff = (ids: WithLanguage<number>) =>
   deleteRequest(`/v2/staff/${ids.ko}/${ids.en}`, { jsessionID: true });
